7c9b2bb0b181f08c0be80e382a739434f626b239
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-02 10:15:54 +0200

    concat unkown args

--- staff/aleix-caminal/js-arrays/concat.js
+++ staff/aleix-caminal/js-arrays/concat.js
@@ -1,16 +1,17 @@
 var btn = document.querySelector('#concat');
 var arr = [1, 2, 3];
+var arr2 = [4, 5, 6];
 
 btn.addEventListener('click', function() {
-    let res = concat(arr, function(num) {
-        console.log(num * 2);
-    });
+    console.log(concat(arr, arr2));
 });
 
-function concat(arr, callback) {
+function concat() {
     let result = [];
-    for (var i = 0; i < arr.length; i++) {
-        result.push(callback(arr[i]));
+    for (var i = 0; i < arguments.length; i++) {
+        for (var j = 0; j < arguments[i].length; j++) {
+            result.push(arguments[i][j]);
+        }
     }
     return result;
 }
