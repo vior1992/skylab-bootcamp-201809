d9be3a543a716dba4c51b125f09d5ebf91474fdc
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-02 16:24:41 +0200

    sort well done (finally)

--- staff/aleix-caminal/js-arrays/sort.js
+++ staff/aleix-caminal/js-arrays/sort.js
@@ -1,16 +1,17 @@
 document.querySelector('#sort').addEventListener('click', function() {
     var months = ['March', 'Jan', 'Feb', 'Dec'];
-    console.log(sort(months));
+    sort(months)
+    console.log(months);
     var nums = [1, 30, 4, 21];
-    console.log(sort(nums));
+    sort(nums)
+    console.log(nums);
 });
 
 function sort(arr) {
-    let result = [];
     for (var i = 0; i < arr.length; i++) {
         var min = i;
         for (var j = i; j < arr.length; j++) {
-            if (arr[j] < arr[min]) {
+            if (('' + arr[j]).localeCompare('' + arr[min]) < 0) {
                 min = j;
             }
         }
@@ -19,5 +20,4 @@ function sort(arr) {
         arr[i] = arr[min];
         arr[min] = sorted;
     }
-    return arr;
 }
