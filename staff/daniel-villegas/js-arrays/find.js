8e4dfc9a37f3a548fcdeda471b0bf618459f5022
vior1992 <vior1992@gmal.com>
2018-10-03 19:47:50 +0200

    try Tdd method with find

--- staff/daniel-villegas/js-arrays/find.js
+++ staff/daniel-villegas/js-arrays/find.js
@@ -1,7 +1,15 @@
 // find.js
 
 function find(arr, callback) {
+    if (!(arr instanceof Array)) throw Error ("Arr is not a array");
+
+    if (!arr.length) throw Error ("Arr is empty");
+
+
+    
     for (var i = 0; i < arr.length; i++) if (callback(arr[i])) return arr[i]; 
 }
+
+
 
 
--- staff/daniel-villegas/js-arrays/for-each.js
+++ staff/daniel-villegas/js-arrays/for-each.js
@@ -1,13 +1,5 @@
 function forEach(arr, callback) {
-    
-    if (typeof arr !== "number") throw Error ("arr is not a number");
-    
-
-    if (!arr.length) throw Error('arr is empty');
-
-
-	if (!arr.trim()) throw Error('arr is blank');
-
+    if (!(arr instanceof Array)) throw Error ("array is not valid");
 
     for (var i = 0; i < arr.length; i++) callback(arr[i], i, arr);
 }
