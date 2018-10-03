089c352616246c5c7de9d727fa23f3eea0fcfce7
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-03 09:42:50 +0200

    everything done except sort

--- staff/maryam-malek/js-arrays/sort.js
+++ staff/maryam-malek/js-arrays/sort.js
@@ -1,32 +1,40 @@
 function sort(arr) {
-    var newArr = [];
-    for(i=0; i<arr.length; i++){
-        for(var x = 0; x<arr.length; x++){
-    
-        if(arr[x]<arr[x+1]){
-            if(arr[x] !== newArr[i])
-            newArr[i] = arr[x];
-        }
-        }
-        return newArr;
-    }    
-    
-    
-    // var aux;
-    // for(i=1; i<arr.length; i++){
-    //     if(arr[i]< arr[0]){
-    //         for(var y = 0;y<arr.length; y++){
-    //             auxAux = arr[y]; 
-    //             aux = arr[0];
-    //             arr[0] = arr[i];
-    //         }
-    //         aux = arr[0];
-    //         arr[0] = arr[i];
-    //         arr[i-1]= aux;
+    // function splice(arr, start, count) {
+    //     var newArr = arr;
+    //     var cutArr = [];
+    //     var sta = start;
+    //     for(i=0;i<count; i++){
+    //         cutArr[i] = arr[start];
+    //         start++;
     //     }
-        
+    //     if((sta + count) < arr.length){
+    //         for(var x=sta; x<(arr.length-count); x++){
+    //             newArr[sta] = arr[sta+count];
+    //             newArr.length = arr.length - count;
+    //         }
+    //     }
+    //     return cutArr;
     // }
+    var orderArr = [];
+    var index = -1;
+    for(var x=0; x<arr.length; x++){
+        orderArr[x] = arr[x];
+    }
+    debugger;
+    for(i=0; i<arr.length; i++){
+        for(var x=0; x<arr.length; x++){
+            if(orderArr[i] >= arr[x]){
+                orderArr[i] = arr[x];
 
-    // return arr;   
+            }
+            index = x;
+        }
+        if(index = -1){
+            index = i;            
+        }
+        arr.splice(index, 1);
+    }
+    return orderArr;
+     
 }
 
