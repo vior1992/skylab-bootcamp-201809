d2f2ada43fa3bcda9e97f812649eba3a0d6b229f
scrafy <scrafy26@gmail.com>
2018-10-03 16:24:44 +0200

    for-each TDD

--- staff/joseluis-juste/js-arrays/for-each.demo.js
+++ staff/joseluis-juste/js-arrays/for-each.demo.js
@@ -4,7 +4,7 @@ console.log('DEMO forEach');
 
 var arr = [1, 2, 3];
 
-forEach(arr, function(num) { console.log(num * 2); });
+forEach(arr, function(num, i, arr) { console.log(num * 2); });
 // 2
 // 4
 // 6
