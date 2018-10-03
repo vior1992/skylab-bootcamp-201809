b3741ce20312614057557b476826cb031c436dbd
Sean Daryanani <seand52@gmail.com>
2018-10-03 08:56:44 +0200

    made small changes

--- staff/sean-daryanani/js-arrays/find-index.js
+++ staff/sean-daryanani/js-arrays/find-index.js
@@ -1,7 +1,7 @@
 function findIndex(arr, elem) {
     var index = 0;
     for (var i=0; i<arr.length; i++) {
-        if (elem(arr[i]) != 0) {
+        if (elem(arr[i])) {
         index = i;
         return index;
         }
