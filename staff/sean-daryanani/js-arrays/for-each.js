8c3ad80eaa4255af6ddab2652c59c18cf6b668d6
Sean Daryanani <seand52@gmail.com>
2018-10-03 17:46:14 +0200

    fixed foreach test

--- staff/sean-daryanani/js-arrays/for-each.js
+++ staff/sean-daryanani/js-arrays/for-each.js
@@ -1,4 +1,7 @@
 function forEach(arr, callback) {
+    if (!(arr instanceof Array)) {
+        throw Error('array is not valid');
+    }
     for (var i=0; i<arr.length; i++) {
         callback(arr[i], i, arr);
     }
