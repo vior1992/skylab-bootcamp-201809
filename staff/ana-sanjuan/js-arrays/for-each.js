b57b896d60e27d31b56137fe6a7dd11177115ed8
adancing <anatsanjuan@gmail.com>
2018-10-03 13:45:36 +0200

    change sort

--- staff/ana-sanjuan/js-arrays/for-each.js
+++ staff/ana-sanjuan/js-arrays/for-each.js
@@ -1,5 +1,5 @@
-// create a function that works as Array.prototype.forEach
 
 function forEach(arr, callback) {
-    for(var i = 0; i < arr.length; i++) callback(arr[i]);
+    for(var i = 0; i < arr.length; i++) 
+        callback(arr[i]);
 } 
--- staff/ana-sanjuan/js-arrays/sort.js
+++ staff/ana-sanjuan/js-arrays/sort.js
@@ -1,24 +1,52 @@
 
-function sort(arr) {
-    var result = [];
-  
-}
+// function sort(arr) {
+//     debugger
+//     var copy = []; var min = 0; 
+//     for(var i = 0; i < arr.length; i++){
+//         copy[i] = arr[i].toString();
+//     }
+    
+//     for (var i = 0; i < copy.length; i++) {
+//         min = copy[i]
+//         for (var j = i; j < copy.length; i++) {
+//             if (min > copy[j]){
+//                 min = copy[j]
+//             }
+//             var temp = copy[i];
+//             copy[i] = copy[j];
+//             copy[j] = temp;
+//         }
+//     }
+// }
+ 
+
+//     // for (var i = 0; i < copy.length; i++){
+//     //     for (var j = 0; j < copy.length; j++){
+//     //         if(copy[j] < copy[j-1]){
+//     //             temp = copy[j];
+//     //             copy[j] = copy[j-1];
+//     //             copy[j-1] = temp
+//     //         }
+//     //     }
+//     // }
+//     // return copy;
+// }
 
 
-console.log('DEMO sort');
+// console.log('DEMO sort');
 
-var months = ['March', 'Jan', 'Feb', 'Dec'];
+// var months = ['March', 'Jan', 'Feb', 'Dec'];
 
-console.log(sort(months));
-// ["Dec", "Feb", "Jan", "March"]
+// console.log(sort(months));
+// // ["Dec", "Feb", "Jan", "March"]
 
-console.log(months);
-// ['March', 'Jan', 'Feb', 'Dec']
+// console.log(months);
+// // ['March', 'Jan', 'Feb', 'Dec']
 
-var nums = [1, 30, 4, 21];
+// var nums = [1, 30, 4, 21];
 
-console.log(sort(nums));
-// [1, 21, 30, 4]
+// console.log(sort(nums));
+// // [1, 21, 30, 4]
 
-console.log(nums);
-// [1, 30, 4, 21]
+// console.log(nums);
+// // [1, 30, 4, 21]
