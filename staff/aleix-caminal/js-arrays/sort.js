86021f1c045ca3ddabc82f6d7ce2fdd4c76c3875
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-03 11:28:47 +0200

    sort improved

--- staff/aleix-caminal/js-arrays/sort.js
+++ staff/aleix-caminal/js-arrays/sort.js
@@ -8,10 +8,10 @@ document.querySelector('#sort').addEventListener('click', function() {
 });
 
 function sort(arr) {
-    for (var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length - 1; i++) {
         var min = i;
         for (var j = i; j < arr.length; j++) {
-            if (('' + arr[j]).localeCompare('' + arr[min]) < 0) {
+            if ('' + arr[j] < '' + arr[min]) {
                 min = j;
             }
         }
