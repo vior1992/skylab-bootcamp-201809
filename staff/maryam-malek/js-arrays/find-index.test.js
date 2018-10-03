438afb7a0c30ee425fc554d8083529623c15b662
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-03 22:17:04 +0200

    work on test

--- staff/maryam-malek/js-arrays/find-index.test.js
+++ staff/maryam-malek/js-arrays/find-index.test.js
@@ -23,42 +23,42 @@ tests.push(function () {
     if (res !== -1) throw Error('returned index does not match with the one expected')
 });
 
-tests.push(function () {
-    console.log('Should succeed on searching inside an array and passing all the specified data to callback');
+// tests.push(function () {
+//     console.log('Should succeed on searching inside an array and passing all the specified data to callback');
     
-    var nums = [4, 26, 33, 105, 10, 25];
-    var numShort = [4, 26];
-    var res;
-    var numbers = [];
-    var index = [];
-    var array = [];
+//     var nums = [4, 26, 33, 105, 10, 25];
+//     var numShort = [4, 26];
+//     var res;
+//     var numbers = [];
+//     var index = [];
+//     var array = [];
     
     
     
-    res = findIndex(nums, function (elem, i, arr) { 
-        debugger;
+//     res = findIndex(nums, function (elem, i, arr) { 
+//         debugger;
         
-        numbers.push(elem);
-        index.push(i);
-        array.push(arr);    
-        return nums >= 25; 
-    });
+//         numbers.push(elem);
+//         index.push(i);
+//         array.push(arr);    
+//         return nums >= 25; 
+//     });
     
-    if (numbers.length !== 2) throw Error('numbers length does not match with the correct one')
-    if (index.length !== 2) throw Error('index length does not match with the correct one')
-    if (array.length !== 2) throw Error('array length does not match with the correct one')
+//     if (numbers.length !== 2) throw Error('numbers length does not match with the correct one')
+//     if (index.length !== 2) throw Error('index length does not match with the correct one')
+//     if (array.length !== 2) throw Error('array length does not match with the correct one')
     
-    numbers.forEach(function(elem, index) {
-        if (elem !== numShort[index]) throw Error('array does not match original one');
-    });
-    index.forEach(function(i, index) {
-        if (i !== index) throw Error('array does not match original one');
-    });
-    array.forEach(function(arr) {
-        if (arr !== numShort) throw Error('array does not match original one');
-    });
+//     numbers.forEach(function(elem, index) {
+//         if (elem !== numShort[index]) throw Error('array does not match original one');
+//     });
+//     index.forEach(function(i, index) {
+//         if (i !== index) throw Error('array does not match original one');
+//     });
+//     array.forEach(function(arr) {
+//         if (arr !== numShort) throw Error('array does not match original one');
+//     });
     
-});
+// });
     
 tests.push(function () {
         console.log('Should fail with non-array input');
@@ -87,6 +87,19 @@ tests.push(function () {
     if (!error) throw Error('has not failed');
 
     if (error.message !== 'entered empty array') throw Error('error message is not correct');
+});
+
+tests.push(function() {
+    console.log('Should fail with null array');
+    var error;
+    var nums = null;
+    try{
+        findIndex(nums, function(num) {return num < 10; });
+    } catch(err) {
+        error = err;
+    }
+    if (!error) throw Error('has not failed');
+    if (error.message !== 'entered invalid array') throw Error('error message is not correct');
 });
 
 tests.push(function () {
