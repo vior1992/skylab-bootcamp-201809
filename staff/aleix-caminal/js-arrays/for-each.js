5d01153df484f3dfc5a7cd1b81005df8e62f3a55
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-04 09:46:12 +0200

    foreach fixed

--- staff/aleix-caminal/js-arrays/for-each.js
+++ staff/aleix-caminal/js-arrays/for-each.js
@@ -11,7 +11,7 @@ function forEach(arr, callback) {
 
     var result = [];
     for (var i = 0; i < arr.length; i++) {
-        result[i] = callback(arr[i]);
+        result[i] = callback(arr[i], i, arr);
     }
     return result;
 }
