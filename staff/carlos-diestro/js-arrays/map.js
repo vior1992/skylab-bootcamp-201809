d3f866da2cdfb6cd9f01a74f69e27c01909a53b5
Carlos Diestro <cardiest@gmail.com>
2018-10-04 14:06:49 +0200

    test map

--- staff/carlos-diestro/js-arrays/map.js
+++ staff/carlos-diestro/js-arrays/map.js
@@ -1,8 +1,11 @@
 function map(array, callback) {
+  if(!(array instanceof Array) || array.length === 0) throw Error('array is not valid');
+  if(typeof callback !== 'function') throw Error('callback is not a function');
+
   var newArray = [];
 
   for(var i = 0; i < array.length; i++) {
-    newArray[i] = callback(array[i]);  
+    newArray[i] = callback(array[i], i, array);  
   }
 
   return newArray;
