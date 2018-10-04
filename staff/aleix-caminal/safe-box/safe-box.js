1d97f87e8f9b470ad0809df0f8695edfa7bbca51
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-04 16:21:35 +0200

    safebox safe

--- staff/aleix-caminal/safe-box/safe-box.js
+++ staff/aleix-caminal/safe-box/safe-box.js
@@ -1,12 +1,23 @@
 // safe-box.js
 
-var safeBox = {
-    saveSecret: function(secret, password) {
-        if (typeof password === 'undefined') throw Error('invalid password');
-        this.secret = {[password]:secret};
-    },
+function safeBoxHandler() {
+    var _secret;
+    var _password;
+    var safeBox = {
+        saveSecret: function (secret, password) {
+            if (typeof password === 'undefined') throw Error('invalid password');
+            _secret = secret;
+            _password = password;
+        },
 
-    retrieveSecret: function(password) {
-        return this.secret[password];
-    },
+        retrieveSecret: function (password) {
+            if (password === _password) {
+                return _secret;
+            }
+            throw Error('invalid password');
+        },
+    };
+    return safeBox;
 }
+
+var safeBox = safeBoxHandler();
