1141b5f54b80f75cbf7883d436e39b2aca47ae5d
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-04 10:27:25 +0200

    find test

--- staff/aleix-caminal/js-arrays/find.js
+++ staff/aleix-caminal/js-arrays/find.js
@@ -8,6 +8,10 @@ document.querySelector('#find').addEventListener('click', function() {
 });
 
 function find(arr, callback) {
+    if (!Array.isArray(arr)) throw Error ("Arr is not a array");
+    if (arr.length <= 0) throw Error ("Arr is empty");
+    if (typeof callback !== "function") throw Error ("Callback isn't a function");
+
     for (var i = 0; i < arr.length; i++) {
         if (callback(arr[i])) {
             return arr[i];
--- staff/aleix-caminal/js-arrays/find.test.js
+++ staff/aleix-caminal/js-arrays/find.test.js
@@ -1,16 +1,69 @@
+var findTests = [];
 document.querySelector('#test-find').addEventListener('click', function() {
-    var arr = [5, 12, 8, 130, 44];
-    var res = find(arr, function(elem) {
-        return elem > 10;
-    });
-
-    console.log(res);
+    testSuite(findTests);
 });
 
-function find(arr, callback) {
-    for (var i = 0; i < arr.length; i++) {
-        if (callback(arr[i])) {
-            return arr[i];
-        }
+findTests.push(function (){
+    console.log("Should give fail if callback is not defined");
+
+    var nums = [5, 12, 8, 130, 44];
+
+    var respuesta = [];
+
+    try {
+        find(nums);
+    } catch(err){
+        respuesta = err;
     }
-}
+
+    if (!respuesta) throw Error ("Should give error, callback not defined");
+
+    if (respuesta.message !== ("Callback isn't a function")) throw Error ("Fail on error message, is not the same");
+
+});
+
+findTests.push(function (){
+    console.log("Should return undefined if not satisfy the provided testing function");
+
+    var nums = [5, 1, 8, 3, 4];
+
+    var respuesta = [];
+
+    respuesta = find(nums, function (elem) { return elem > 10; });
+
+    if (respuesta !== undefined) throw Error ("The output should be undefined");
+
+});
+
+findTests.push(function (){
+    console.log("Should return error for empty array");
+
+    var nums = [];
+
+    var respuesta = [];
+
+    try {
+        find(nums, function (elem) { respuesta = elem > 10; });
+    } catch(err) {
+        respuesta = err
+    }
+
+    if (!respuesta) throw Error ("The array should be empty");
+
+    if (respuesta.message !== "Arr is empty") throw Error ("Fail on error message, is not the same");
+
+});
+
+findTests.push(function (){
+    console.log("Should return 12");
+
+    var nums = [5, 12, 8, 130, 44];
+
+    var respuesta = [];
+
+
+    respuesta = find(nums, function (elem) { return elem > 10; });
+
+    if (respuesta !== 12) throw Error ("The output should be 12");
+
+});
