0ce7c7f2b662e0b0b3226fbe771be84760eee40e
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-04 11:48:32 +0200

    'update'

--- staff/gianluca-bragaglia/js-arrays/includes.js
+++ staff/gianluca-bragaglia/js-arrays/includes.js
@@ -1,5 +1,17 @@
 
 function includes(arr, elem) {
+
+    if (!(arr instanceof Array)) throw Error('array is not valid');
+
+    if ( typeof elem === 'undefined') throw Error('element is not defined');
+
+    if( typeof elem === 'function') throw Error('element is a function, must be a string or number or object or boolean');
+
+    if (!elem.trim()) throw Error('element is blank');
+
+    if(arr.length == 0) throw Error('array is empty');
+
+
     for(var i=0; i<arr.length; i++) {
         if(elem === arr[i]) {
             return true
