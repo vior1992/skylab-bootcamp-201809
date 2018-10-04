47a19049e41c3af105372c014e3cf83cd8cc3d75
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-10-04 11:40:23 +0200

    fix fill

--- staff/judith-gongora/js-arrays/fill.demo.js
+++ staff/judith-gongora/js-arrays/fill.demo.js
@@ -7,6 +7,6 @@ console.log(fill(arr, 5, 1)); // [1, 5, 5, 5]
 console.log(fill(arr, 6)); // [6, 6, 6, 6]
 
 console.log('testing');
-// console.log(fill(o));
+console.log(fill(o));
 // console.log(fill());
 
