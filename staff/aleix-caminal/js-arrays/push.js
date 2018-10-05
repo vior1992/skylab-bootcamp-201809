a1b4561b12cf00f4b27121629f48b13e1543b339
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-05 10:27:55 +0200

    push

--- staff/aleix-caminal/js-arrays/push.js
+++ staff/aleix-caminal/js-arrays/push.js
@@ -5,5 +5,9 @@ document.querySelector('#push').addEventListener('click', function() {
 });
 
 function push(arr, elem) {
+    if (!Array.isArray(arr)) throw Error('array is not valid');
+    if (arr.length <= 0) throw Error('array is empty');
+    if (!elem) throw Error('element not defined');
+
     arr[arr.length] = elem;
 }
