6c7e7650eec3587be3b0d3da9e6c03a743694933
Sean Daryanani <seand52@gmail.com>
2018-10-05 11:46:43 +0200

    added tests

--- staff/sean-daryanani/js-arrays/index-of.js
+++ staff/sean-daryanani/js-arrays/index-of.js
@@ -1,7 +1,11 @@
 function indexOf(arr, word) {
-    if (arr===undefined) {
-        throw Error ('array is not valid');
+    if(!(arr instanceof Array)) {
+        throw Error (typeof arr + ' is not an array');
     }
+    if (!arr.length) {
+        throw Error ('one argument is empty');
+    }
+
     var indexPosition = 0;
     for (var i=0; i<arr.length; i++) {
         if (arr[i]===word) {
