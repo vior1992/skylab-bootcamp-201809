2a4516cb4cb26cb4686f08089a0811e994cd8ea2
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-05 09:36:16 +0200

    every test

--- staff/aleix-caminal/js-arrays/every.js
+++ staff/aleix-caminal/js-arrays/every.js
@@ -8,6 +8,8 @@ document.querySelector('#every').addEventListener('click', function() {
 });
 
 function every(arr, callback) {
+    if (!Array.isArray(arr)) throw Error('array is not valid');
+    if (typeof callback !== 'function') throw Error('callback is not a function');
     for (var i = 0; i < arr.length; i++) {
         if (!callback(arr[i])) {
             return false;
