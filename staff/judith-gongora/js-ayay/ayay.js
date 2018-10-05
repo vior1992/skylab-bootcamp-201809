2a21a5c33fbba55b5ec88271902926c672e6e601
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-10-05 16:10:26 +0200

    add ayay

---
+++ staff/judith-gongora/js-ayay/ayay.js
@@ -0,0 +1,48 @@
+/**
+ * ?
+ * 
+ */
+function Ayay() {
+    this.length = 0;
+}
+
+// TODO
+
+Ayay.prototype.push = function(element) {
+    this[this.length] = element;
+    this.length++;
+};
+
+Ayay.prototype.pop = function() {
+    var del;
+    if (this.length>0){
+        del = this[this.length-1];
+        delete this[this.length-1]
+        this.length--;
+    }else{
+        del = 'undefined';
+    }
+    
+};
+
+Ayay.prototype.forEach = function(callback) {
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    for (var i = 0; i < this.length; i++) callback(this[i], i, this);
+};
+
+Ayay.prototype.map = function(callback) {
+    // TODO
+};
+
+Ayay.prototype.sort = function() {
+    // TODO
+};
+
+Ayay.prototype.filter = function(callback) {
+    // TODO
+};
+
+Ayay.prototype.find = function(callback) {
+    // TODO
+};
