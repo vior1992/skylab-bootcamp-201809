ce8fe117cd8569b108c78fc05b312d5237d810e9
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-05 19:39:45 +0200

    initial jasmine done

--- staff/maryam-malek/js-ayay/ayay.js
+++ staff/maryam-malek/js-ayay/ayay.js
@@ -12,11 +12,11 @@ Ayay.prototype.push = function(element) {
     this[this.length] = element;
 
     this.length++;
+    return length;
 };
 
 Ayay.prototype.pop = function() {
     var extra = this[this.length-1];
-    var newArr = this;
     delete this[this.length-1];
     this.length--;
     return extra;
@@ -29,17 +29,45 @@ Ayay.prototype.forEach = function(callback) {
 };
 
 Ayay.prototype.map = function(callback) {
-    // TODO
+    
+    var newAy = new Ayay;
+    for(var i = 0; i<this.length; i++){
+        newAy.push(callback(this[i], i));
+    }
+    return newAy;
 };
 
 Ayay.prototype.sort = function() {
-    // TODO
+    var index;
+    for(var i = 0; i<this.length; i++){
+        index = i;
+        for(var x = i; x<this.length; x++){
+            if(''+this[x] < ''+this[index]){
+                index = x;
+            }
+        }
+        var moment = this[i];
+        this[i]=this[index];
+        this[index]=moment;
+    }
+    return this;
 };
 
 Ayay.prototype.filter = function(callback) {
-    // TODO
+    var newAy = new Ayay;
+    for(var i = 0; i<this.length; i++){
+       if(callback(this[i], i)){
+        newAy.push(this[i]);
+       }
+        
+    }
+    return newAy;
 };
 
 Ayay.prototype.find = function(callback) {
-    // TODO
+    for(var i = 0; i<this.length; i++){
+        if(callback(this[i], i)){
+            return this[i];
+        }
+    }
 };
---
+++ staff/maryam-malek/js-ayay/ayay.spec.js
@@ -0,0 +1,133 @@
+describe('Ayay', function () {
+    var ayay;
+
+    beforeEach(function() {
+        ayay = new Ayay;
+    });
+
+    describe('push', function () {
+        it('should push items', function () {
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+
+            expect(ayay.length).toEqual(3);
+
+            for (var i = 0; i < ayay.length; i++)
+                expect(ayay[i]).toEqual(i + 1);
+        });
+    });
+
+    describe('forEach', function () {
+        it('should iterate on valid ayay', function () {
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+
+            var result = [];
+
+            ayay.forEach(function (elem, index) { result[index] = elem * 2; });
+
+            expect(result.length).toEqual(ayay.length);
+
+            result.forEach(function (elem, index) {
+                expect(elem).toEqual(ayay[index] * 2);
+            });
+        });
+    });
+
+    describe('pop', function () {
+        it('should iterate on valid ayay', function () {
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+
+            var last = ayay[ayay.length-1];
+            var result = ayay.pop();
+            
+            expect(ayay.length).toEqual(2);
+            expect(result).toEqual(last);
+            
+        });
+        
+    });
+    describe('map', function () {
+        it('should succeed on iterating an array and multiply by 2', function () {
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+
+            var result = ayay.map(function(elem, i){return elem * 2});
+            var comprovation = [2,4,6];
+
+            expect(result).toBeTruthy();
+
+            for(var i = 0; i<ayay.length; i++){
+                expect(result[i]).toEqual(comprovation[i]);
+            }
+
+            expect(result.length).toEqual(ayay.length);
+            
+        });
+        
+    });
+    describe('filter', function () {
+        it('should succeed on returning an array with the elements that pass the callback requirement', function () {
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+
+
+            var result = ayay.filter(function(elem, i){return elem > 2});
+            var comprovation = [3];
+            expect(result).toBeTruthy();
+
+            for(var i = 0; i<ayay.length; i++){
+                expect(result[i]).toEqual(comprovation[i]);
+            }
+
+            expect(result.length).toEqual(comprovation.length);
+            
+        });
+        
+    });
+
+    describe('find', function () {
+        it('should succeed on returning the first element that pass the callback requirement', function () {
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+
+
+            var result = ayay.find(function(elem){return elem > 1});
+            var comprovation = 2;
+            expect(result).toBeTruthy();
+
+            expect(result).toEqual(comprovation);
+
+            expect(result.length).toEqual(comprovation.length);
+            
+        });
+        
+    });
+
+    describe('sort', function () {
+        it('should succeed on sorting the elements of the array', function () {
+            ayay.push(1);
+            ayay.push(3);
+            ayay.push(2);
+
+
+            var result = ayay.sort();
+            var comprovation = [1,2,3];
+            expect(result).toBeTruthy();
+
+            for(var i = 0; i<ayay.length; i++){
+                expect(result[i]).toEqual(comprovation[i]);
+            }
+            expect(result.length).toEqual(comprovation.length);
+            
+        });
+        
+    });
+});
