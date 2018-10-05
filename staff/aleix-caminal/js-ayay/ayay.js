1228b46c2ec6f8f435cd0002ad15355e745dc91e
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-05 15:51:03 +0200

    copied methods

--- staff/aleix-caminal/js-ayay/ayay.js
+++ staff/aleix-caminal/js-ayay/ayay.js
@@ -1,6 +1,6 @@
 /**
  * ?
- * 
+ *
  */
 function Ayay() {
     this.length = 0;
@@ -15,7 +15,9 @@ Ayay.prototype.push = function(element) {
 };
 
 Ayay.prototype.pop = function() {
-    // TODO
+    var result = this[this.length - 1];
+    this.length--;
+    return result;
 };
 
 Ayay.prototype.forEach = function(callback) {
@@ -25,17 +27,48 @@ Ayay.prototype.forEach = function(callback) {
 };
 
 Ayay.prototype.map = function(callback) {
-    // TODO
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    var result = [];
+    for (var i = 0; i < this.length; i++) {
+        result[i] = callback(this[i], i, this);
+    }
+    return result;
 };
 
 Ayay.prototype.sort = function() {
-    // TODO
+    for (var i = 0; i < this.length - 1; i++) {
+        var min = i;
+        for (var j = i; j < this.length; j++) {
+            if ('' + this[j] < '' + this[min]) {
+                min = j;
+            }
+        }
+
+        var sorted = this[i];
+        this[i] = this[min];
+        this[min] = sorted;
+    }
 };
 
 Ayay.prototype.filter = function(callback) {
-    // TODO
+    if (typeof callback !== 'function') throw Error('callback is not a function');
+
+    var result = [];
+    for (var i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            result[result.length] = this[i];
+        }
+    }
+    return result;
 };
 
 Ayay.prototype.find = function(callback) {
-    // TODO
+    if (typeof callback !== "function") throw Error ("Callback isn't a function");
+
+    for (var i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            return this[i];
+        }
+    }
 };
