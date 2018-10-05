0479c8e379b7c7fa1d4af558e29d32dc099740af
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-05 16:07:44 +0200

    initial work

---
+++ staff/maryam-malek/js-ayay/ayay.js
@@ -0,0 +1,47 @@
+/**
+ * ?
+ * 
+ */
+function Ayay() {
+    this.length = 0;
+}
+
+// TODO
+
+Ayay.prototype.push = function(element) {
+    this[this.length] = element;
+
+    this.length++;
+};
+
+Ayay.prototype.pop = function() {
+    var extra = this[this.length-1];
+    var newArr = this;
+    for(var i=0; i<this.length-2; i++){
+        newArr[i] = this[i]; 
+    }
+    this.length--;
+    return extra;
+};
+
+Ayay.prototype.forEach = function(callback) {
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    for (var i = 0; i < this.length; i++) callback(this[i], i, this);
+};
+
+Ayay.prototype.map = function(callback) {
+    // TODO
+};
+
+Ayay.prototype.sort = function() {
+    // TODO
+};
+
+Ayay.prototype.filter = function(callback) {
+    // TODO
+};
+
+Ayay.prototype.find = function(callback) {
+    // TODO
+};
