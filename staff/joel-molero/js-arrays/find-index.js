bc06fbfbd44aafacf3b5bfaa5b3c4cefd6cf81e0
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-10-05 11:46:06 +0200

    find-index-test added

--- staff/joel-molero/js-arrays/find-index.js
+++ staff/joel-molero/js-arrays/find-index.js
@@ -1,7 +1,19 @@
 function findIndex(arr,elem){
+
+if (!arr.length) throw Error("array is empty");
+
+if (typeof array[index] !== 'undefined') 
+
+
+
+
+
+
     for(x=0;x<arr.length;x++){
         if(elem(arr[x])){
             return x;
         }
+        if()
     }
+
 }
