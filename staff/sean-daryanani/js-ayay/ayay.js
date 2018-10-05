1a68a55ddffda8ad09d52958a700ccf9f97c758b
Sean Daryanani <seand52@gmail.com>
2018-10-05 16:07:41 +0200

    add ayay stuff

---
+++ staff/sean-daryanani/js-ayay/ayay.js
@@ -0,0 +1,44 @@
+/**
+ * ?
+ * 
+ */
+function Ayay() {
+    this.length = 0;
+}
+
+// TODO
+
+Ayay.prototype.push = function(element) {
+    this[this.length] = element;
+
+    this.length++;
+};
+
+Ayay.prototype.pop = function() {
+    var popped = this[this.length-1];
+    //delete last property
+    this.length = this.length-1;
+    return popped;
+};
+
+Ayay.prototype.forEach = function(callback) {
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    for (var i = 0; i < this.length; i++) callback(this[i], i, this);
+};
+
+Ayay.prototype.map = function(callback) {
+    // TODO
+};
+
+Ayay.prototype.sort = function() {
+    // TODO
+};
+
+Ayay.prototype.filter = function(callback) {
+    // TODO
+};
+
+Ayay.prototype.find = function(callback) {
+    // TODO
+};
