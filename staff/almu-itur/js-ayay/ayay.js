674ffba370d1180fdda9fe40459bccbb5ef65a8a
Almu Iturgoyen <almudena.iturgoyen@gmail.com>
2018-10-06 18:06:20 +0200

    changes ayay

--- staff/almu-itur/js-ayay/ayay.js
+++ staff/almu-itur/js-ayay/ayay.js
@@ -8,35 +8,134 @@ function Ayay() {
 
 // TODO
 
-Ayay.prototype.push = function(element) {
+Ayay.prototype.push = function (element) {
     this[this.length] = element;
 
     this.length++;
 };
 
-Ayay.prototype.pop = function() {
-    element = this[this.length];
-    this.length--; 
+
+
+Ayay.prototype.pop = function () {
+    var element = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+
+    return element;
 };
 
-Ayay.prototype.forEach = function(callback) {
+Ayay.prototype.forEach = function (callback) {
     if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
     for (var i = 0; i < this.length; i++) callback(this[i], i, this);
 };
 
-Ayay.prototype.map = function(callback) {
-    // TODO
+Ayay.prototype.map = function (callback) {
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    var result = new Ayay;
+    //var result = [];
+
+    for (var i = 0; i < this.length; i++)
+        //result[i] = callback(this[i], i, this);
+        //result.length++;
+        result.push(callback(this[i], i, this));
+
+    return result;
 };
 
-Ayay.prototype.sort = function() {
-    // TODO
+
+Ayay.prototype.sort = function () {
+
+    if (!(this instanceof Ayay)) throw Error('array is not valid');
+    if (this.length <= 0) throw Error('array is empty');
+
+    for (var i = 0; i < this.length - 1; i++) {
+        var min = i;
+        for (var j = i; j < this.length; j++) {
+            if ('' + this[j] < '' + this[min]) {
+                min = j;
+            }
+        }
+
+        var sorted = this[i];
+        this[i] = this[min];
+        this[min] = sorted;
+    }
+    return this;
+
+    /*
+    var indexMain = 0;
+    var indexSecondary = 0;
+    var indexTemp = 0;
+    var tempItem = [];
+    var arraySorted = [];
+    var arrString = [];
+    var swap = false;
+    
+        
+        for (var i = 0; i < this.length; i++) {
+            arrString[i] = this[i].toString();
+        }
+        
+        console.log(this);
+
+        for(indexMain = 0; indexMain < arrString.length; indexMain++) {
+            
+            tempItem = arrString[indexMain];
+    
+            for(indexSecondary = indexMain + 1; indexSecondary < arrString.length; indexSecondary++) {
+                
+                if(arrString[indexSecondary] < tempItem) {
+                    tempItem = arrString[indexSecondary];
+                    indexTemp = indexSecondary;
+                    swap = true;
+                }
+            }
+            if (swap) { 
+                arraySorted[indexMain] = tempItem;
+                arrString[indexTemp] = arrString[indexMain];
+                swap = false;
+            }
+            else { arraySorted[indexMain] = arrString[indexMain]; }
+        }
+        this = arraySorted;
+        return this;
+    */
 };
 
-Ayay.prototype.filter = function(callback) {
-    // TODO
+
+Ayay.prototype.filter = function (callback) {
+
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+
+    var index = 0;
+    var indexResult = 0;
+    //var result = [];
+    var result = new Ayay;
+    var flag = false;
+
+    for (index = 0; index < this.length; index++) {
+
+        flag = callback(this[index]);
+
+        if (flag === true) {
+            result.push(this[index]);
+            indexResult++;
+        }
+    }
+
+    return result;
 };
 
-Ayay.prototype.find = function(callback) {
-    // TODO
+Ayay.prototype.find = function (element) {
+
+    var flag = false;
+
+    for (index = 0; index < this.length; index++) {
+
+        if (element === this[index]) { return this[index]; }
+    }
+
+    return undefined;
 };
--- staff/almu-itur/js-ayay/ayay.spec.js
+++ staff/almu-itur/js-ayay/ayay.spec.js
@@ -3,6 +3,7 @@ describe('Ayay', function () {
 
     beforeEach(function() {
         ayay = new Ayay;
+        
     });
 
     describe('push', function () {
@@ -17,7 +18,7 @@ describe('Ayay', function () {
                 expect(ayay[i]).toEqual(i + 1);
         });
     });
-
+    
     describe('forEach', function () {
         it('should iterate on valid ayay', function () {
             ayay.push(1);
@@ -35,4 +36,96 @@ describe('Ayay', function () {
             });
         });
     });
+
+    describe('pop', function () {
+        it('should pop items', function () {
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+            var lastItem = ayay.pop();
+
+            expect(ayay.length).toEqual(2);
+        });
+    });
+
+    describe('map', function () {
+        it('should map items', function () {
+            //var result = new Ayay;
+            var result = [];
+
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+            
+            ayay.map(function (element, index) {
+                result[index] = element*2;
+                //result.map(element * 2);
+            });
+            
+            expect(result.length).toEqual(ayay.length);
+
+            result.forEach(function (elem, index) {
+                expect(elem).toEqual(ayay[index] * 2);
+            });
+        });
+
+    });
+
+    describe('sort', function () {
+        it('should sort items on array', function () {
+            
+            ayay.push(1);
+            ayay.push(30);
+            ayay.push(4);
+            ayay.push(21);
+            
+            ayay.sort();
+            
+            expect(ayay.length).toEqual(4);
+            expect(ayay[0]).toEqual(1);
+            expect(ayay[1]).toEqual(21);
+            expect(ayay[2]).toEqual(30);
+            expect(ayay[3]).toEqual(4);
+        });
+    });
+
+    describe('filter', function () {
+        it('should filter items', function () {
+            var result = new Ayay;
+
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+                  
+            result = ayay.filter(isGreaterThan);
+            function isGreaterThan(item) {
+                return item > 2
+            }
+
+            expect(result.length).toEqual(1);
+
+            result.forEach(function (elem, index) {
+                expect(elem).toBeGreaterThan(2);
+            });
+            
+        });
+        
+    });
+
+    describe('find', function () {
+        it('should find items', function () {
+            var result = [];
+
+            ayay.push(1);
+            ayay.push(2);
+            ayay.push(3);
+                  
+            result = ayay.find(2);
+
+            expect(result).toBe(2);
+            
+        });
+        
+    });
+
 });
