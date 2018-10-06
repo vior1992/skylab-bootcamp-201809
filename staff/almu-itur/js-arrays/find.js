9876f8e7293e096c4a1445b303fbb9fe36e0fa98
Almu Iturgoyen <almudena.iturgoyen@gmail.com>
2018-10-06 18:09:46 +0200

    changes arrays

--- staff/almu-itur/js-arrays/find.js
+++ staff/almu-itur/js-arrays/find.js
@@ -1,16 +1,11 @@
 function find(nums, callback) {
-    var index = 0;
-    var value = 0
+   
     var flag = false;
    
     for(index=0; index<nums.length; index++) {
 
-        flag = callback(nums[index]);
-
-        if(flag == true) {
-            return nums[index];
-            index = nums.length-1;
-        }
+        if (element ===nums[index]) { return nums[index]; }
     }
-    if (flag == false) { return undefined; }
+
+    return undefined;
 }
