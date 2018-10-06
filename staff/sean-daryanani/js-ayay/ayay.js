d2ec7bad0addd6276502124aa57bc230ee1e8a46
Sean Daryanani <seand52@gmail.com>
2018-10-06 17:47:02 +0200

    added ayay tests

--- staff/sean-daryanani/js-ayay/ayay.js
+++ staff/sean-daryanani/js-ayay/ayay.js
@@ -14,12 +14,13 @@ Ayay.prototype.push = function() {
         pointer[pointer.length] = el;
         pointer.length++;
     })
+    return this.length;
 };
 
 Ayay.prototype.pop = function() {
     var popped = this[this.length-1];
     delete this[this.length-1];
-    this.length = this.length-1;
+    if (this.length>0) this.length = this.length-1;
     return popped;
 };
 
@@ -30,21 +31,58 @@ Ayay.prototype.forEach = function(callback) {
 };
 
 Ayay.prototype.map = function(callback) {
-    var newArr = [];
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+    var newArr = new Ayay();
     for (var i=0; i<this.length; i++) {
-        newArr[i] = callback(this[i]);
+        newArr.push(callback(this[i],i, this));
     }
     return newArr;
 };
 
 Ayay.prototype.sort = function() {
-    // TODO
+    for (var i=0; i < this.length-1; i++) {
+        for (var j=0; j< this.length; j++) {
+            if (this[j] > this[j+1]) {
+                var temp = this[j];
+                this[j] = this[j+1];
+                this[j+1] = temp;
+            }
+        }
+    }
+    return this;
 };
 
 Ayay.prototype.filter = function(callback) {
-    // TODO
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+    var newArr= new Ayay();
+    for (var i=0; i<this.length; i++) {
+        if (callback(this[i])) {
+            newArr.push(this[i])
+        }
+    }
+    return newArr;
 };
 
 Ayay.prototype.find = function(callback) {
-    // TODO
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+    for (var i=0; i<this.length; i++) {
+        if (callback(this[i])) return this[i];
+    }
 };
+
+Ayay.prototype.join = function(element) {
+    var outputString = this[0];
+    if (element === undefined) {
+        for (var i = 1; i < this.length; i++) {
+            outputString += "," + this[i];
+        }
+    } else {
+        for (var i = 1; i < this.length; i++) {
+            outputString += element + this[i];
+        }
+    }
+    if (!this.length) {
+        return outputString = "";
+    }
+    return outputString;
+}
