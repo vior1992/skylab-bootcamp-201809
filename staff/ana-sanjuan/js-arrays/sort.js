8f57c20d9f1fc71c901cbfb4feb7c4c0c5619f09
adancing <anatsanjuan@gmail.com>
2018-10-06 11:49:39 +0200

    add sort

--- staff/ana-sanjuan/js-arrays/sort.js
+++ staff/ana-sanjuan/js-arrays/sort.js
@@ -1,52 +1,15 @@
 
-// function sort(arr) {
-//     debugger
-//     var copy = []; var min = 0; 
-//     for(var i = 0; i < arr.length; i++){
-//         copy[i] = arr[i].toString();
-//     }
-    
-//     for (var i = 0; i < copy.length; i++) {
-//         min = copy[i]
-//         for (var j = i; j < copy.length; i++) {
-//             if (min > copy[j]){
-//                 min = copy[j]
-//             }
-//             var temp = copy[i];
-//             copy[i] = copy[j];
-//             copy[j] = temp;
-//         }
-//     }
-// }
- 
+function sort(arr) {
+    var temp;
 
-//     // for (var i = 0; i < copy.length; i++){
-//     //     for (var j = 0; j < copy.length; j++){
-//     //         if(copy[j] < copy[j-1]){
-//     //             temp = copy[j];
-//     //             copy[j] = copy[j-1];
-//     //             copy[j-1] = temp
-//     //         }
-//     //     }
-//     // }
-//     // return copy;
-// }
-
-
-// console.log('DEMO sort');
-
-// var months = ['March', 'Jan', 'Feb', 'Dec'];
-
-// console.log(sort(months));
-// // ["Dec", "Feb", "Jan", "March"]
-
-// console.log(months);
-// // ['March', 'Jan', 'Feb', 'Dec']
-
-// var nums = [1, 30, 4, 21];
-
-// console.log(sort(nums));
-// // [1, 21, 30, 4]
-
-// console.log(nums);
-// // [1, 30, 4, 21]
+    for (var j = 0; j < arr.length -1; j++){
+        for (var i = 0; i < arr.length; i++){
+            if (arr[i] > arr[i +1]){
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
