19f5fd59a9c1cac1c106f6537feb8e137c804fc9
henry <henry.r.novoa@gmail.com>
2018-10-07 15:00:07 +0200

    30 specs for jasmine

--- staff/henry-novoa/js-ayay/ayay.js
+++ staff/henry-novoa/js-ayay/ayay.js
@@ -5,13 +5,12 @@
 function Ayay() {
     this.length = 0;
 }
-
-// TODO
-
-
-
+ 
 
 Ayay.prototype.push = function(element) {
+     
+if(!(arguments.length)) throw Error('an element to push is required');
+    
     this[this.length] = element;
 
     this.length++;
@@ -19,7 +18,8 @@ Ayay.prototype.push = function(element) {
 
 Ayay.prototype.pop = function() {
     
-    
+    if(arguments.length) throw Error('input not required');
+    if(!(this.length)) throw Error('ayay is empty');
     var res;
     var res = this[this.length -1];
     delete this[this.length -1];
@@ -29,14 +29,18 @@ Ayay.prototype.pop = function() {
 };
 
 Ayay.prototype.forEach = function(callback) {
+    if(!(arguments.length)) throw Error('input is empty');
+    if(typeof callback !== 'function') throw Error(callback +' is not a function');
+    if(!(this.length)) throw Error('ayay is empty');
     if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
     for (var i = 0; i < this.length; i++) callback(this[i], i, this);
 };
 
 Ayay.prototype.map = function(callback) {
-    
-
+    if(!(arguments.length)) throw Error('input is empty');
+    if(typeof callback !== 'function') throw Error(callback +' is not a function');
+    if(!(this.length)) throw Error('ayay is empty');
     var result = new Ayay;
     for(var i = 0 ; i<this.length;i++){
         result[i] = callback(this[i],i,this);
@@ -50,6 +54,10 @@ Ayay.prototype.map = function(callback) {
 
 Ayay.prototype.sort = function() {
    
+
+    if(arguments.length) throw Error('input not required');
+    if(!(this.length)) throw Error('ayay is empty');
+    
     var temp;
     for(var i = 0; i<this.length;i++){
         this[i]=this[i].toString();
@@ -74,15 +82,15 @@ Ayay.prototype.sort = function() {
 
 Ayay.prototype.filter = function(callback) {
     
-
+    if(!(arguments.length)) throw Error('input is empty');
+    if(typeof callback !== 'function') throw Error(callback +' is not a function');
+    if(!(this.length)) throw Error('ayay is empty');
     index= -1;
     var result = new Ayay;
-    //var resultIndex = 0;
+
     for(var i=0; i < this.length; i++){
         if(callback(this[i],i) ===true){
             result.push(this[i]);
-            //result[resultIndex] = this[i]; 
-            //resultIndex++;    
         }
     }
     
@@ -93,7 +101,10 @@ Ayay.prototype.filter = function(callback) {
 };
 
 Ayay.prototype.find = function(callback) {
-        
+     
+    if(!(arguments.length)) throw Error('input is empty');
+    if(typeof callback !== 'function') throw Error(callback +' is not a function');
+    if(!(this.length)) throw Error('ayay is empty');
     for(var i=0;i<this.length;i++){ 
     if(callback(this[i],i)){
         return this[i];
