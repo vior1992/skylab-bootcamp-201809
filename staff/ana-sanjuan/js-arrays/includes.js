3fdf4aab51dcdace711d983625c607d395437ca0
adancing <anatsanjuan@gmail.com>
2018-10-07 18:37:18 +0200

    add reverse and include tests

--- staff/ana-sanjuan/js-arrays/includes.js
+++ staff/ana-sanjuan/js-arrays/includes.js
@@ -1,5 +1,9 @@
 
 function includes(arr, elem) {
+    if (!arr.length) throw Error('array is empty')
+    
+    if (elem === undefined) throw Error('element is not defined')
+    
     for (var i = 0; i < arr.length; i ++) {
         if (arr[i] === elem) return true;
     }
