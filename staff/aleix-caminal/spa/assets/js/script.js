2cb6663ae62a5f91a55590942338bb0fd57199ab
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-08 18:16:23 +0200

    workflow

--- staff/aleix-caminal/spa/assets/js/script.js
+++ staff/aleix-caminal/spa/assets/js/script.js
@@ -4,15 +4,12 @@ function Landing(title, tag, registerCallback, loginCallback) {
     this.register = document.createElement('button');
     this.register.innerText = 'Register';
     this.register.addEventListener('click', registerCallback);
-
     this.element.appendChild(this.register);
-
     this.element.appendChild(document.createTextNode(' or '));
 
     this.login = document.createElement('button');
-    this.login.innerText = 'Login';
+    this.login.innerText = 'Log In';
     this.login.addEventListener('click', loginCallback);
-
     this.element.appendChild(this.login);
 }
 
@@ -21,27 +18,57 @@ Landing.prototype.constructor = Landing;
 
 function Login(title, tag, loginCallback) {
     Panel.call(this, title, tag);
-
     this.element.style.display = 'none';
 
     this.form = document.createElement('form');
-
     this.element.appendChild(this.form);
-
     this.username = document.createElement('input');
-
     this.form.appendChild(this.username);
-
     this.password = document.createElement('input');
-
     this.form.appendChild(this.password);
 
     this.login = document.createElement('button');
-
+    this.login.type = 'button';
+    this.login.innerText = 'Log In';
+    this.login.addEventListener('click', loginCallback);
     this.form.appendChild(this.login);
 }
 
 Login.prototype = Object.create(Panel.prototype);
 Login.prototype.constructor = Login;
 
-// TODO Register & Welcome
+function Register(title, tag, registerCallback) {
+    Panel.call(this, title, tag);
+    this.element.style.display = 'none';
+
+    this.form = document.createElement('form');
+    this.element.appendChild(this.form);
+    this.username = document.createElement('input');
+    this.form.appendChild(this.username);
+    this.password = document.createElement('input');
+    this.form.appendChild(this.password);
+
+    this.register = document.createElement('button');
+    this.register.type = 'button';
+    this.register.innerText = 'Register';
+    this.register.addEventListener('click', registerCallback);
+    this.form.appendChild(this.register);
+}
+
+Register.prototype = Object.create(Panel.prototype);
+Register.prototype.constructor = Register;
+
+function Welcome(title, tag, logoutCallback) {
+    Panel.call(this, title, tag);
+    this.element.style.display = 'none';
+
+
+    this.logout = document.createElement('button');
+    this.logout.type = 'button';
+    this.logout.innerText = 'Log Out';
+    this.logout.addEventListener('click', logoutCallback);
+    this.element.appendChild(this.logout);
+}
+
+Welcome.prototype = Object.create(Panel.prototype);
+Welcome.prototype.constructor = Welcome;
--- staff/aleix-caminal/spa/main.js
+++ staff/aleix-caminal/spa/main.js
@@ -1,16 +1,31 @@
 var landing = new Landing('Choose an option', 'section',
     function() {
-        // TODO on register click
+        landing.hide();
+        register.show();
     },
-    
+
     function() {
         landing.hide();
         login.show();
     }
 );
 
+var login = new Login('Login', 'section', function() {
+    login.hide();
+    welcome.show();
+});
+
+var register = new Register('Register', 'section', function() {
+    register.hide();
+    login.show();
+});
+
+var welcome = new Welcome('Welcome', 'section', function() {
+    welcome.hide();
+    login.show();
+});
+
 document.body.appendChild(landing.element);
-
-var login = new Login('Login', 'section');
-
 document.body.appendChild(login.element);
+document.body.appendChild(register.element);
+document.body.appendChild(welcome.element);
