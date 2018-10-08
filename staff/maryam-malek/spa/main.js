759701c1c7e0ae1d4d244ae1ba3c56d3d8a4500f
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-08 19:14:04 +0200

    first files

---
+++ staff/maryam-malek/spa/main.js
@@ -0,0 +1,33 @@
+var landing = new Landing('Choose an option', 'section',
+    function() {
+        landing.hide();
+        register.show();
+    },
+    function() {
+        landing.hide();
+        login.show();
+    });
+
+document.body.appendChild(landing.element);
+
+var login = new Login('Login', 'section', function() {
+    login.hide();
+    welcome.show();
+});
+
+document.body.appendChild(login.element);
+
+
+var register = new Register('Register', 'section', function() {
+        register.hide();
+        login.show();
+});
+
+document.body.appendChild(register.element);
+
+var welcome = new Welcome('Welcome', 'section', function() {
+    welcome.hide();
+    landing.show();
+});
+
+document.body.appendChild(welcome.element);
