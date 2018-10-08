5e75450f37bdda020448f5052a9e1b482d4d9c5a
Fede <fedegomezcucchiararo@gmail.com>
2018-10-08 11:35:06 +0200

    change feature/js-ayay

--- staff/fede-gomez/js-ayay/ayay.js
+++ staff/fede-gomez/js-ayay/ayay.js
@@ -16,6 +16,7 @@ Ayay.prototype.push = function(element) {
 };
 
 Ayay.prototype.forEach = function(callback) {
+
     if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
     for (var i = 0; i < this.length; i++) callback(this[i], i, this);
@@ -30,7 +31,18 @@ Ayay.prototype.pop = function() {
 };
 
 Ayay.prototype.map = function(callback) {
-    // TODO
+    var newAyay = new Ayay;
+
+    //We have to iterate through the original ayay and return the values that passed the criteria in the callback
+    
+    for(var i=0; i<this.length; i++){
+        if(callback(this[i], i)){newAyay.push = this[i]}
+        console.log('Element with index: ' + i + ' and value: ' + this[i]);
+    }
+
+    console.log(newAyay);
+
+    return newAyay;
 };
 
 Ayay.prototype.sort = function() {
