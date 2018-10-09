1129a806093780b591df3376bc34c7c8ca506cf7
Tomás Sánchez Navarro <tsanchez.navarro@gmail.com>
2018-10-09 13:28:18 +0200

    added functionality

--- staff/tomas-sancheznavarro/spa/main.js
+++ staff/tomas-sancheznavarro/spa/main.js
@@ -1,14 +1,31 @@
 var landing = new Landing('Choose an option', 'section',
-    function() {
-        // TODO on register click
+    function () {
+        landing.hide();
+        register.show();
+
     },
-    function() {
+    function () {
         landing.hide();
         login.show();
     });
 
 document.body.appendChild(landing.element);
 
-var login = new Login('Login', 'section');
+var login = new Login('Login', 'section', function () {
+    login.hide();
+    welcome.show();
 
+});
 document.body.appendChild(login.element);
+
+var register = new Register('Register', 'section', function () {
+    register.hide();
+    login.show();
+});
+document.body.appendChild(register.element);
+
+var welcome = new Welcome('Welcome', 'section', function () {
+    login.hide();
+    welcome.show();
+});
+document.body.appendChild(welcome.element);
