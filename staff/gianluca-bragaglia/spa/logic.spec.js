a90f8e3004f4e2345ee6f4fdd7f7a0a4fc1ee36a
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-09 18:34:59 +0200

    update spa

--- staff/gianluca-bragaglia/spa/logic.spec.js
+++ staff/gianluca-bragaglia/spa/logic.spec.js
@@ -1,1 +1,41 @@
 // TODO
+
+describe('spa/logic', function () {
+    var user;
+    user = {
+        email: email,
+        username: username,
+        password: password
+    };
+    var email;
+    var username;
+    var password;
+
+
+    describe('register', function () {
+        it('should succeed if username and password are valid', function () {
+
+            logic.login('gi@gio.it', 'gia', '123', 
+                function(){
+
+            },
+            function(message) {
+                alert(message);
+            });
+
+            expect(user.password).toEqual('123');
+            expect(user.username).toEqual('gia');
+        }); 
+        
+       /*  it('should fail if no username is passed', function () {
+            user = {
+                username: undefined,
+                password: 123
+            };
+
+            expect(function(){
+                logic.login();
+            }).toThrowError('invalid username');
+        }); */
+    });
+});
