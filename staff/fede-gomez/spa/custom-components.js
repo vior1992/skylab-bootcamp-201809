8a08b9c4289d2a6b2851c24eebaae567889a761b
Fede <fedegomezcucchiararo@gmail.com>
2018-10-09 13:17:05 +0200

    update feature/spa

--- staff/fede-gomez/spa/custom-components.js
+++ staff/fede-gomez/spa/custom-components.js
@@ -2,6 +2,7 @@ function Landing(title, tag, registerCallback, loginCallback) {
     Panel.call(this, title, tag);
 
     this.register = document.createElement('button');
+    this.register.className = 'btn btn-success';
     this.register.innerText = 'Register';
     this.register.addEventListener('click', registerCallback);
 
@@ -10,6 +11,7 @@ function Landing(title, tag, registerCallback, loginCallback) {
     this.element.appendChild(document.createTextNode(' or '));
 
     this.login = document.createElement('button');
+    this.login.className = 'btn btn-primary';
     this.login.innerText = 'Login';
     this.login.addEventListener('click', loginCallback);
 
@@ -31,17 +33,75 @@ function Login(title, tag, loginCallback) {
     this.username = document.createElement('input');
 
     this.form.appendChild(this.username);
+    this.username.placeholder = "username";
 
     this.password = document.createElement('input');
 
     this.form.appendChild(this.password);
+    this.password.placeholder = "password";
+    this.password.type = "password";
 
     this.login = document.createElement('button');
 
     this.form.appendChild(this.login);
+    this.login.innerText = "ok";
+    this.login.className = "btn btn-primary";
 }
 
 Login.prototype = Object.create(Panel.prototype);
 Login.prototype.constructor = Login;
 
 // TODO Register & Welcome
+
+function Register (title, tag, registerCallback ) {
+    Panel.call(this, title, tag);
+
+    this.element.style.display = 'none';
+
+    this.form = document.createElement('form');
+    // this.form.addEventListener('submit', function(event){
+    //     event.preventDefault();
+
+
+
+    // }.bind(this));
+
+    this.element.appendChild(this.form);
+
+    this.name = document.createElement('input');
+    this.form.appendChild(this.name);
+    this.name.placeholder = "Name";
+
+    this.surname = document.createElement('input');
+    this.form.appendChild(this.surname);
+    this.surname.placeholder = "Surname";
+
+    this.username = document.createElement('input');
+    this.form.appendChild(this.username);
+    this.username.placeholder = "New username";
+
+    this.password = document.createElement('input');
+    this.form.appendChild(this.password);
+    this.password.placeholder = "New password";
+
+    this.password = document.createElement('input');
+    this.form.appendChild(this.password);
+    this.password.placeholder = "Repeat password";
+
+    this.register = document.createElement('button');
+    this.form.appendChild(this.register);
+    this.register.innerText = "ok";
+    this.register.className = "btn btn-primary";
+}
+
+Register.prototype = Object.create(Panel.prototype);
+Register.prototype.constructor = Register;
+
+
+function Welcome (title, tag) {
+    Panel.call(this, title, tag);
+
+    this.element.style.display = 'none';
+
+
+}
--- staff/fede-gomez/spa/main.js
+++ staff/fede-gomez/spa/main.js
@@ -1,9 +1,23 @@
+/**
+ * 
+ * 
+ * 
+ * Everything in here represents the "Business Logic" and the "Presentation Logic"
+ *
+ * 
+ * 
+ * 
+ * */
+
 var landing = new Landing('Choose an option', 'section',
     function() {
-        // TODO on register click
+        landing.hide();
+        login.hide();
+        register.show();
     },
     function() {
         landing.hide();
+        register.hide();
         login.show();
     });
 
@@ -12,3 +26,11 @@ document.body.appendChild(landing.element);
 var login = new Login('Login', 'section');
 
 document.body.appendChild(login.element);
+
+var register = new Register ('Register', 'section');
+
+document.body.appendChild(register.element);
+
+var welcome = new Welcome ('Welcome', 'section');
+
+document.body.appendChild(welcome.element);
