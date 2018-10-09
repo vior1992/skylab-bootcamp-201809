fbb0b24ccfa5700abfc75f94345784e553c60c2c
Sergio <sergio.luz.fernandez@gmail.com>
2018-10-09 18:52:59 +0200

    second MANU version

--- staff/sergio-luz/spa/spa-sergio/logic.spec.js
+++ staff/sergio-luz/spa/spa-sergio/logic.spec.js
@@ -1,7 +1,16 @@
 // TODO
+
+
 describe('Save data in safeBox', function(){
-    it('Password should be set', function(){
-        var _password="";
-        expect(_password).toBeTruthy();
+    it('Data should be set', function(){
+        var name='sergio';
+        var surname='luz';
+        var username='slf';
+        var password='olala';
+        var repeatPass='olala';
+
+        var result = safeBox.saveData(name, surname, username, password, repeatPass);
+
+        expect(result).toEqual(true);
     })
 });
