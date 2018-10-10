392ce1878dab7dbff548b4b797c7c47642edde2e
Tomás Sánchez Navarro <tsanchez.navarro@gmail.com>
2018-10-10 12:12:54 +0200

    extended logic.js and added jasmine tests

--- staff/tomas-sancheznavarro/spa/logic.js
+++ staff/tomas-sancheznavarro/spa/logic.js
@@ -2,10 +2,13 @@ var user;
 
 var logic = {
     register: function (name, surname, username, password, onSuccess, onFail) {
-        if (!name || !name.trim().length) onFail('invalid name');
-        else if (!surname || !surname.trim().length) onFail('invalid surname');
-        else if (!username || !username.trim().length) onFail('invalid username');
-        else if (!password || !password.trim().length) onFail('invalid password');
+
+        if (typeof name !== 'string' || !name || !name.trim().length) onFail('invalid name');
+        else if (typeof surname !== 'string' || !surname || !surname.trim().length) onFail('invalid surname');
+        else if (typeof username !== 'string' || !username || !username.trim().length) onFail('invalid username');
+        else if (typeof password !== 'string' || !password || !password.trim().length) onFail('invalid password');
+        else if (typeof onSuccess !== 'function') throw TypeError(onSuccess + ' is not a function');
+        else if (typeof onFail !== 'function') throw TypeError(onFail + ' is not a function');
         else {
             user = {
                 name: name,
@@ -19,11 +22,17 @@ var logic = {
     },
 
     login: function (username, password, onSuccess, onFail) {
-        if (!username || !username.trim().length) onFail('invalid username');
-        else if (!password || !password.trim().length) onFail('invalid password');
+        if (typeof username !== 'string' || !username || !username.trim().length) onFail('invalid username');
+        else if (typeof password !== 'password' || !password || !password.trim().length) onFail('invalid password');
+        else if (typeof onSuccess !== 'function') throw TypeError(onSuccess + ' is not a function');
+        else if (typeof onFail !== 'function') throw TypeError(onFail + ' is not a function');
         else if (user) {
             if (user.username === username && user.password === password) {
-                onSuccess(user);
+                onSuccess({
+                    name: user.name,
+                    surname: user.name,
+                    username: user.username
+                });
             } else onFail('wrong credentials!');
         }
     }
--- staff/tomas-sancheznavarro/spa/logic.spec.js
+++ staff/tomas-sancheznavarro/spa/logic.spec.js
@@ -1,28 +1,103 @@
 describe('logic', function () {
-    var user;
 
-    beforeEach(function () {
-        user = {
-            name: 'Tomás',
-            surname: 'Sánchez',
-            username: 'calvino',
-            password: '123'
-        };
+
+    describe('register', function () {
+        beforeEach(function () {
+            user = undefined;
+        });
+        it('should succeed in correct data', function () { //onSuccess
+            logic.register('John', 'Doe', 'jd', '123', function () {
+                    expect(user.name).toEqual('John');
+                    expect(user.surname).toEqual('Doe');
+                    expect(user.username).toEqual('jd');
+                    expect(user.password).toEqual('123');
+                },
+                function (message) { //onFail
+                    //expect(message).not.toBeDefined();
+                    throw Error(message);
+                }
+            );
+        });
+
+        it('should fail on undefined name', function () { //onSuccess
+            logic.register(undefined, 'Doe', 'jd', '123', function () {
+                    throw Error();
+                },
+                function (message) { //onFail
+                    expect(message).toEqual('invalid name');
+                }
+
+            );
+        });
+
+        it('should fail on empty name', function () { //onSuccess
+            logic.register('', 'Doe', 'jd', '123', function () {
+                    throw Error();
+                },
+                function (message) { //onFail
+                    expect(message).toEqual('invalid name');
+                }
+
+            );
+        });
+
+        it('should fail on blank name', function () { //onSuccess
+            logic.register('         ', 'Doe', 'jd', '123', function () {
+                    throw Error();
+                },
+                function (message) { //onFail
+                    expect(message).toEqual('invalid name');
+                }
+            );
+        });
+
+        it('should fail on non-string name (number)', function () { //onSuccess
+            logic.register(123, 'Doe', 'jd', '123', function () {
+                    throw Error();
+                },
+                function (message) { //onFail
+                    expect(message).toEqual('invalid name');
+                }
+            );
+        });
+
+        it('should fail on non-string name (boolean)', function () { //onSuccess
+            logic.register(true, 'Doe', 'jd', '123', function () {
+                    throw Error();
+                },
+                function (message) { //onFail
+                    expect(message).toEqual('invalid name');
+                }
+            );
+        });
+
+        // TODO apply analog cases: surname, username, and password
+
     });
 
     describe('login', function () {
-        it('should push one or more items', function () {
-            ayay.push(1, 2, 3);
-
-            expect(ayay.length).toEqual(3);
-
-            for (var i = 0; i < ayay.length; i++)
-                expect(ayay[i]).toEqual(i + 1);
+        beforeEach(function () {
+            user = {
+                name: 'John',
+                surname: 'Doe',
+                username: 'jd',
+                password: '123'
+            };
         });
 
-        it('should continue being an ayay', function () {
-            ayay.push(1, 2, 3);
-            expect(ayay instanceof Ayay).toBe(true);
+        it('should succeed on correct credentials', function () {
+            logic.login('jd', '123', function (user) {
+                    expect(user).toBeDefined();
+                    expect(user.name).toEqual('John');
+                    expect(user.surname).toEqual('Doe');
+                    expect(user.username).toEqual('jd');
+                    expect(user.password).toEqual('123');
+                },
+                function (message) {
+                    throw Error(message);
+                }
+            );
         });
+
     });
 });
