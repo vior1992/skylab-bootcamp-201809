dc3e6a3cce4555a3ad97c7a2a8d4035357d4436d
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-10 15:07:24 +0200

    components in jquery

--- staff/aleix-caminal/web-components/assets/js/components.js
+++ staff/aleix-caminal/web-components/assets/js/components.js
@@ -12,151 +12,68 @@ Component.prototype.hide = function () {
 
 function Panel(title, tag) {
     Component.call(this, tag);
-    this.element.className = 'panel';
+    $(this.element).addClass('panel');
 
     this.title = document.createElement('h2');
-    this.title.innerText = title;
-    this.title.className = 'panel__title';
+    $(this.title).html(title);
+    $(this.title).addClass('panel__title');
 
-    this.element.appendChild(this.title);
+    $(this.element).append(this.title);
 }
 
 Panel.prototype = Object.create(Component.prototype);
 Panel.prototype.constructor = Panel;
 
-function Dialog(title, text, tag) {
-    Panel.call(this, title, tag);
-
-    this.element.className = 'dialog';
-
-    this.title.className = 'dialog__title';
-
-    this.body = document.createElement('p');
-    this.body.innerText = text;
-    this.body.className = 'dialog__body';
-
-    this.element.appendChild(this.body);
-}
-
-Dialog.prototype = Object.create(Panel.prototype);
-Dialog.prototype.constructor = Dialog;
-
-function Alert(title, text, tag, callback, error) {
-    Dialog.call(this, title, text, tag);
-
-    this.element.className = error ? 'alert alert--danger' : 'alert';
-
-    this.title.className = 'alert__title';
-
-    this.body.className = 'alert__body';
-
-    this.accept = document.createElement('button');
-    this.accept.innerText = 'Accept';
-
-    // var self = this;
-
-    // this.accept.addEventListener('click', function(event) {
-    //     self.element.style.display = 'none';
-    // });
-
-    this.accept.addEventListener('click', function () {
-        this.element.style.display = 'none';
-
-        callback();
-    }.bind(this));
-
-    this.accept.className = 'alert__button';
-
-    this.element.appendChild(this.accept);
-}
-
-Alert.prototype = Object.create(Dialog.prototype);
-Alert.prototype.constructor = Alert;
-
-function Confirm(title, text, tag, acceptCallback, cancelCallback) {
-    Dialog.call(this, title, text, tag);
-
-    this.element.className = 'confirm';
-
-    this.title.className = 'confirm__title';
-
-    this.body.className = 'confirm__body';
-
-    this.cancel = document.createElement('button');
-    this.cancel.innerText = 'Cancel';
-    this.cancel.className = 'confirm__button';
-
-    this.cancel.addEventListener('click', function () {
-        this.element.style.display = 'none';
-
-        cancelCallback();
-    }.bind(this));
-
-    this.element.appendChild(this.cancel);
-
-    this.accept = document.createElement('button');
-    this.accept.innerText = 'Accept';
-    this.accept.className = 'confirm__button confirm__button--accept';
-
-    this.accept.addEventListener('click', function () {
-        this.element.style.display = 'none';
-
-        acceptCallback();
-    }.bind(this));
-
-    this.element.appendChild(this.accept);
-}
-
-Confirm.prototype = Object.create(Dialog.prototype);
-Confirm.prototype.constructor = Confirm;
-
 function Form(id, elements) {
     this.form = document.createElement('form');
-    if (id) this.form.id = id;
-    this.form.className = 'form';
+    if (id) $(this.form).attr('id', id)
+    $(this.form).addClass('form');
 
     function formatLabel(label) {
         var words = label.split('_');
-        words.forEach(function(word, i) {
+        $.each(words, function(i, word) {
             words[i] = word.charAt(0).toUpperCase() + word.slice(1);
         });
         return words.join(' ');
     }
 
     var that = this;
-    elements.forEach(function(element) {
+    $.each(elements, function(i, element) {
         switch (element.element) {
             case 'input':
                 that.group = document.createElement('div');
-                that.group.className = 'form__group';
-
-                that.label = document.createElement('label');
-                if (element.id) that.label.for = element.id;
-                that.label.className = element.class ? element.class : 'form-group__label';
-                that.label.innerText = element.label ? element.label : formatLabel(element.id);
-                that.group.appendChild(that.label);
+                $(that.group).addClass('form__group');
 
                 that.input = document.createElement('input');
-                if (element.id) that.input.id = element.id;
-                that.input.className = element.class ? element.class : 'form-group__input';
-                that.input.type = element.type ? element.type : 'text';
-                that.group.appendChild(that.input);
+                if (element.id) $(that.input).attr('id', element.id);
+                $(that.input).addClass(element.class ? element.class : 'form-group__input');
+                $(that.input).prop('type', element.type ? element.type : 'text');
+                $(that.group).append(that.input);
 
-                that.form.appendChild(that.group);
+                that.label = document.createElement('label');
+                if (element.id) $(that.label).attr('for', element.id);
+                $(that.label).addClass(element.class ? element.class : 'form-group__label');
+                $(that.label).html(element.label ? element.label : formatLabel(element.id));
+                $(that.group).prepend(that.label);
+
+                $(that.form).append(that.group);
                 break;
             case 'button':
                 if (typeof that.buttons === 'undefined') {
                     that.buttons = document.createElement('div');
-                    that.buttons.className = 'form__buttons'
-                    that.form.appendChild(that.buttons);
+                    $(that.buttons).addClass('form__buttons');
+                    $(that.form).append(that.buttons);
                 }
 
                 that.button = document.createElement('button');
-                that.button.type = element.type ? element.type : 'button';
-                if (element.label) that.button.innerText = element.label;
-                that.button.className = element.class ? element.class : 'form-buttons__button';
-                for (var listener in element.on) that.button.addEventListener(listener, element.on[listener]);
-                that.buttons.appendChild(that.button);
+                $(that.button).prop('type', element.type ? element.type : 'button');
+                if (element.label) $(that.button).html(element.label);
+                $(that.button).addClass(element.class ? element.class : 'form-buttons__button');
+                $.each(element.on, function(listener, callback) {
+                    $(that.button).on(listener, callback);
+                });
+
+                $(that.buttons).append(that.button);
                 break;
         }
     });
