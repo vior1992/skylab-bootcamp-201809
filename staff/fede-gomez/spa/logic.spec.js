d94ce0b82f7ce645cea561d951da5929a23bbddc
Fede <fedegomezcucchiararo@gmail.com>
2018-10-10 10:38:21 +0200

    update logic.spec.js

--- staff/fede-gomez/spa/logic.spec.js
+++ staff/fede-gomez/spa/logic.spec.js
@@ -1,1 +1,29 @@
-// TODO
+describe('SPA', function () {
+
+    beforeEach(function() {
+        //Something to execute before each describe
+    });
+
+    describe('register', function () {
+        it('register the correct values', function () {
+
+            var name = 'Fede';
+            var surname = 'Gomez';
+            var username = 'Username';
+            var password = '123';
+            
+            var expectedName = name;
+            var expectedSurname = surname;
+            var expectedUsername = username;
+            var expectedPassword = password;
+
+            logic.register(name, surname, username, password);
+
+           expect(user.name).toEqual(expectedName);
+           expect(user.surname).toEqual(expectedSurname);
+           expect(user.username).toEqual(expectedUsername);
+           expect(user.password).toEqual(expectedPassword);
+        });
+        
+    });
+});
