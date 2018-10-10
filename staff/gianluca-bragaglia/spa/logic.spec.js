286d8e9a29331c8b7162bfb69fa60c2629529298
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-10 10:17:51 +0200

    spa testing

--- staff/gianluca-bragaglia/spa/logic.spec.js
+++ staff/gianluca-bragaglia/spa/logic.spec.js
@@ -1,36 +1,184 @@
 // TODO
 
-describe('spa/logic', function () {
-    var user;
-    user = {
-        email: email,
-        username: username,
-        password: password
-    };
-    var email;
-    var username;
-    var password;
+describe('logic', function () {
+    
+    
+    describe('register', function () {
+        
+        beforeEach(function () {
+            email = 'gia@Ga.com';
+            username = 'gian';
+            password = '123'; 
+        });
 
+        var email;
+        var username;
+        var password;
+        
+        it('should fail on undefined email', function () {
+            email = undefined;
+            var msg;
+            logic.register(email, username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid email');
+        });
+        it('should fail on undefined username', function () {
+            username = undefined;
+            var msg;
+            logic.register(email, username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid username');
+        });
+        it('should fail on undefined password', function () {
+            password = undefined;
+            var msg;
+            logic.register(email, username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid password');
+        });
+        it('should fail on blanck username', function () {
+            username = '   ';
+            var msg;
+            logic.register(email, username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid username');
+        });
+        it('should fail on blanck email', function () {
+            email = '   ';
+            var msg;
+            logic.register(email, username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid email');
+        });
+        it('should fail on blanck password', function () {
+            password = '   ';
+            var msg;
+            logic.register(email, username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid password');
+        });
+        it('should succeed on keeping user data', function(){
+
+            var error;
+            logic.register(email, username, password, function(){
+                error = false
+            }, function(){
+                error = true;
+            });
+            expect(error).toEqual(false);
+            expect(user.email).toEqual(email);
+            expect(user.username).toEqual(username);
+            expect(user.password).toEqual(password);
+        });
+      
+
+    });
 
     describe('login', function () {
-        it('should succeed if username and password are valid', function () {
-
-            logic.login('gi@gio.it', 'gia', '123'); 
-               
-
-            expect(user.password).toEqual('123');
-            expect(user.username).toEqual('gia');
-        }); 
         
-       /*  it('should fail if no username is passed', function () {
-            user = {
-                username: undefined,
-                password: 123
-            };
+        beforeEach(function () {
+            email = 'gia@Ga.com';
+            username = 'gian';
+            password = '123'; 
+        });
 
-            expect(function(){
-                logic.login();
-            }).toThrowError('invalid username');
-        }); */
-    });
-});
+        it('should fail on undefined username', function () {
+            var username = undefined;
+            var password;
+            var msg;
+            logic.login(username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid username');
+        }); 
+        it('should fail on blank username', function () {
+            var username = '    ';
+            var password;
+            var msg;
+            logic.login(username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid username');
+        }); 
+        it('should fail on undefined password', function () {
+            var username = 'gian';
+            var password = undefined;
+            var msg;
+            logic.login(username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid password');
+        });
+        it('should fail on blank password', function () {
+            var username = 'gian';
+            var password = '    ';
+            var msg;
+            logic.login(username, password, function () {
+            }, function (message) {
+                msg = message;
+            });
+            expect(msg).toEqual('invalid password');
+        }); 
+        it('should succeed on correct username and password', function () {
+
+            logic.register('e', 'gian', '123', function () { }, function () { })
+            var username = 'gian';
+            var password = '123';
+            var error;
+            logic.login(username, password, function () {
+                error = false;
+            }, function (message) {
+                error = true;
+            });
+            expect(error).toEqual(false);
+        });
+        it('should fail on incorrect username', function () {
+
+            logic.register('p', 'gian', '123', function () { }, function () { })
+            var username = 'gol';
+            var password = '123';
+            var error;
+            var msg;
+            logic.login(username, password, function () {
+                error = false;
+            }, function (message) {
+                error = true;
+                msg = message
+            });
+            expect(error).toEqual(true);
+            expect(msg).toEqual('wrong credentials!');
+        }); 
+        it('should fail on incorrect password', function () {
+
+            logic.register('p', 'gian', '123', function () { }, function () { })
+            var username = 'gian';
+            var password = '13';
+            var error;
+            var msg;
+            logic.login(username, password, function () {
+                error = false;
+            }, function (message) {
+                error = true;
+                msg = message
+            });
+            expect(error).toEqual(true);
+            expect(msg).toEqual('wrong credentials!');
+        }); 
+    }); 
+
+}); 
