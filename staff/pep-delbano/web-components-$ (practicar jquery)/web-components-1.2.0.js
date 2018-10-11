46615722d52cd087b5eada1ebbcb0d9a763ca543
pepdbm7 <pepdbm7@gmail.com>
2018-10-11 15:23:50 +0200

    jquery some changes made

--- staff/pep-delbano/web-components-$ (practicar jquery)/web-components-1.2.0.js
+++ staff/pep-delbano/web-components-$ (practicar jquery)/web-components-1.2.0.js
@@ -2,57 +2,66 @@ function Component(tag) {
     this.element = document.createElement(tag);
 };
 
+
 Component.prototype.show = function () {
     $(this.element).css('display','block');
 };
 
+
 Component.prototype.hide = function () {
     $(this.element).css('display','none');
 };
+
 
 function Panel(title, tag) {
     Component.call(this, tag);
 
     this.element.addClass("panel");
 
-    $('.panel').append("<h2></h2>");
-    this.title.innerText = title;
-    this.element.addClass('panel__title'); 
-
-    this.element.appendChild(this.title);
+    this.title = $('<h2></h2>').text(title).addClass('panel__title');
+    
+    this.title.appendTo(title.body);
 };
 
 Panel.prototype = Object.create(Component.prototype);
 Panel.prototype.constructor = Panel;
 
+
 function Dialog(title, text, tag) {
     Panel.call(this, title, tag);
 
-    this.element.className = 'dialog';
+    this.element.addClass('dialog');
 
-    this.title.className = 'dialog__title';
+    this.title = $('<h2></h2>').text(title).addClass('dialog__title');
 
-    this.body = document.createElement('p');
-    this.body.innerText = text;
-    this.body.className = 'dialog__body';
+    this.text = $('<p></p>').text(text).addClass('dialog__body');
 
-    this.element.appendChild(this.body);
+    this.title.appendTo(title.body);
+
+    this.text.appendTo(this.body);
 }
 
 Dialog.prototype = Object.create(Panel.prototype);
 Dialog.prototype.constructor = Dialog;
+
+
 
 function Alert(title, text, tag, callback, error) {
     Dialog.call(this, title, text, tag);
 
     this.element.className = error ? 'alert alert--danger' : 'alert';
 
-    this.title.className = 'alert__title';
+    this.title = $('<h2></h2>').text(title).addClass('alert__title');
 
-    this.body.className = 'alert__body';
+    this.text = $('<p></p>').text(text).addClass('alert__body');
 
-    this.accept = document.createElement('button');
-    this.accept.innerText = 'Accept';
+    this.accept = $('<button></button>').text('Accept').addClass('confirm__button--accept');
+
+    $('.alert__title').appendTo(this.body);
+
+    $(.'alert__body').appendTo(this.body);
+
+    $('.confirm__button--accept').appendTo(this.body);
 
     // var self = this;
 
@@ -60,52 +69,51 @@ function Alert(title, text, tag, callback, error) {
     //     self.element.style.display = 'none';
     // });
 
-    this.accept.addEventListener('click', function () {
-        this.element.style.display = 'none';
+    $('.alert__title').on('click', function () {
+        $(this.element).css('display','none');
 
         callback();
     }.bind(this));
 
-    this.accept.className = 'alert__button';
+    this.accept.addClass('alert__button');
 
-    this.element.appendChild(this.accept);
+    this.accept.apendTo(this.body);
 }
 
 Alert.prototype = Object.create(Dialog.prototype);
 Alert.prototype.constructor = Alert;
 
+
+
 function Confirm(title, text, tag, acceptCallback, cancelCallback) {
     Dialog.call(this, title, text, tag);
 
-    this.element.className = 'confirm';
+    this.element.addClass('confirm');
 
-    this.title.className = 'confirm__title';
+    this.title.addClass('confirm__title');
 
-    this.body.className = 'confirm__body';
+    this.body.addClass('confirm__body');
 
-    this.cancel = document.createElement('button');
-    this.cancel.innerText = 'Cancel';
-    this.cancel.className = 'confirm__button';
+    this.cancel = $('<button></button>').text('Cancel').addClass('confirm__button');
 
-    this.cancel.addEventListener('click', function () {
+    $('.confirm__button').on('click', function () {
         this.element.style.display = 'none';
 
         cancelCallback();
     }.bind(this));
 
-    this.element.appendChild(this.cancel);
+    $('.confirm__button').appendTo(this.body);
 
-    this.accept = document.createElement('button');
-    this.accept.innerText = 'Accept';
-    this.accept.className = 'confirm__button confirm__button--accept';
+        
+    this.accept = $('<button></button>').text('Accept').addClass('confirm__button--accept');
 
-    this.accept.addEventListener('click', function () {
+    $('.confirm__button--accept').on('click', function () {
         this.element.style.display = 'none';
 
         acceptCallback();
     }.bind(this));
 
-    this.element.appendChild(this.accept);
+    $('.confirm__button--accept').appendTo(this.body);
 }
 
 Confirm.prototype = Object.create(Dialog.prototype);
