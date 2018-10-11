ac69c1e66e0f2f8d9f34eaba6ee1dd48f3544b5a
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-11 09:40:02 +0200

    small modification

--- staff/maryam-malek/js-arrays/fill.js
+++ staff/maryam-malek/js-arrays/fill.js
@@ -1,24 +1,31 @@
 function fill(arr, value, start, end) {
-    var newArr = [];
-
-    if(end && start){
-        for(i=0;i<start; i++){
-            newArr[i] = arr[i];
-        }
-        for(var x=start; x<end; x++){
-            newArr[x] = value;
-        }
-    }else if(!end && start){
-        for(i=0;i<start; i++){
-            newArr[i] = arr[i];
-        }
-        for(var x=start; x<arr.length; x++){
-            newArr[x] = value;
-        } 
-    }else{
-        for(i=0;i<arr.length; i++){
-            newArr[i] = value;
-        }
+    
+    
+    for (var i = ((start)? start: 0) ; i < (end? end: arr.length); i++){
+        arr[i] = value;
     }
-    return newArr;
+    return arr
+    
+    // var newArr = [];
+    
+    // if(end && start){
+    //     for(i=0;i<start; i++){
+    //         newArr[i] = arr[i];
+    //     }
+    //     for(var x=start; x<end; x++){
+    //         newArr[x] = value;
+    //     }
+    // }else if(!end && start){
+    //     for(i=0;i<start; i++){
+    //         newArr[i] = arr[i];
+    //     }
+    //     for(var x=start; x<arr.length; x++){
+    //         newArr[x] = value;
+    //     } 
+    // }else{
+    //     for(i=0;i<arr.length; i++){
+    //         newArr[i] = value;
+    //     }
+    // }
+    // return newArr;
 }
