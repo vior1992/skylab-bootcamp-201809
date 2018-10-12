8dc30c2af2e597ef2725203543bd3cf0139b71db
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-12 23:10:12 +0200

    add tracks

--- staff/gianluca-bragaglia/spotify-app/logic.js
+++ staff/gianluca-bragaglia/spotify-app/logic.js
@@ -15,7 +15,7 @@ const logic = {
 
             xhr.open('get', 'https://api.spotify.com/v1/search?type=artist&query=' + query)
 
-            var token = 'BQCupPYx5P0fkoO9-am_w077fH0FujhUdOt-MiEtjaXxERbFoXOWPehgh5cZB_ch2QNoT9CqL2PkzUI7t4Vk4b43lc2Nqv0KzSVnopFc8LZBTPa8jRyG79gMOSSURKJwmc_ad71-osnYXw'
+            var token = 'BQDBgQe5OKOv59B-9XZARPBRqCzA-GssSL5CwB69_RGiIu2tQRXVlgvGruIguNrp_8WjANbFSYfbNwUdHYePVfpGHXss8gJUMVW2-HNzIPRsghpxam-E52l1m4Jc8yGTk6AI_w5Kec3hPw'
 
             xhr.setRequestHeader('authorization', 'Bearer ' + token)
 
@@ -41,7 +41,33 @@ const logic = {
 
             xhr.open('get', 'https://api.spotify.com/v1/artists/' + id + '/albums')
 
-            var token = 'BQAI6-FVuO-JBI9krzovJ_uwIrFYpabgvRTnzqTAs-6g6ig9-aHS7PECGm87T6fLHpU3caVHkR8Is3sv9nwpi7R3okXhYWdmQCdmW3-XB2MbJVzRR13IVakGZhaFeD3HP55wSHNcpfubQg'
+            var token = 'BQDBgQe5OKOv59B-9XZARPBRqCzA-GssSL5CwB69_RGiIu2tQRXVlgvGruIguNrp_8WjANbFSYfbNwUdHYePVfpGHXss8gJUMVW2-HNzIPRsghpxam-E52l1m4Jc8yGTk6AI_w5Kec3hPw'
+
+            xhr.setRequestHeader('authorization', 'Bearer ' + token)
+
+            xhr.send()
+        })
+    },
+
+    searchTracks(idTracks) {
+        return new Promise((resolve, reject) => {
+            var xhr = new XMLHttpRequest()
+
+            xhr.addEventListener('load', function() {
+                var res = JSON.parse(xhr.responseText)
+
+                resolve(res.items)
+                console.log(res.items);
+
+            })
+
+            xhr.addEventListener('error', function() {
+                reject() // TODO
+            })
+
+            xhr.open('get', 'https://api.spotify.com/v1/albums/' + idTracks + '/tracks')
+
+            var token = 'BQDBgQe5OKOv59B-9XZARPBRqCzA-GssSL5CwB69_RGiIu2tQRXVlgvGruIguNrp_8WjANbFSYfbNwUdHYePVfpGHXss8gJUMVW2-HNzIPRsghpxam-E52l1m4Jc8yGTk6AI_w5Kec3hPw'
 
             xhr.setRequestHeader('authorization', 'Bearer ' + token)
 
--- staff/gianluca-bragaglia/spotify-app/main.js
+++ staff/gianluca-bragaglia/spotify-app/main.js
@@ -1,8 +1,10 @@
 const $artists = $('.artists')
 const $albums = $('.albums')
+const $tracks = $('.tracks')
 
 $artists.hide()
 $albums.hide()
+$tracks.hide()
 
 const $form = $('form')
 
@@ -21,7 +23,7 @@ $form.submit(event => {
 })
 
 function listArtist(artists) {
-    $artists.show()
+    $artists.css('display', 'block')
 
     const $ul = $artists.find('ul')
 
@@ -31,20 +33,16 @@ function listArtist(artists) {
         const $a = $(`<a href="#">${artist.name}</a>`)
 
         $a.click(() => {
-            event.preventDefault()
-
-            // TODO search albums by artist id
 
             const id = artist.id
-            console.log(id);
+
+            $albums.css('display', 'block')
 
             logic.searchAlbums(id)
                 .then(albums => {
-                    listArtist(albums)
+                    listAlbums(albums)
                 })
                 .catch(console.error)
-
-
         })
 
         const $li = $('<li>')
@@ -52,18 +50,16 @@ function listArtist(artists) {
         $li.append($a)
 
         $ul.append($li)
+
     })
 }
 
 
 function listAlbums(albums) {
-    $albums.show()
 
-    const $ulAlbums = $('.list-albums')
-    const $liAlbums = $ulAlbums.find('li')
-    const $aAlbums = $ulAlbums.find('a')
-
-    $ulAlbums.empty()
+    let $ulAlbums = $albums.find('ul')
+    let $liAlbums = $ulAlbums.find('li')
+    let $aAlbums = $liAlbums.find('a')
 
     albums.forEach(album => {
 
@@ -72,12 +68,52 @@ function listAlbums(albums) {
         $aAlbums.click(() => {
             event.preventDefault()
 
+            const idTracks = album.id
+
+            $tracks.css('display', 'block')
+
+            logic.searchTracks(idTracks)
+                .then(tracks => {
+                    listTracks(tracks)
+                })
+                .catch(console.error)
 
         })
+
+        $liAlbums = $('<li>')
+
+        $liAlbums.append($aAlbums)
+
+        $ulAlbums.append($liAlbums)
+
     })
-    $liAlbums = $('<li>')
 
-    $liAlbums.append($aAlbums)
 
-    $ulAlbums.append($liAlbums)
+}
+
+function listTracks(tracks) {
+
+    let $ulTracks = $tracks.find('ul')
+    let $liTracks = $ulTracks.find('li')
+    let $aTracks = $liTracks.find('a')
+
+    tracks.forEach(tracks => {
+
+        $aTracks = $(`<a href="#">${tracks.name}</a>`)
+
+        $aTracks.click(() => {
+            event.preventDefault()
+
+
+        })
+
+        $liTracks = $('<li>')
+
+        $liTracks.append($aTracks)
+
+        $ulTracks.append($liTracks)
+
+    })
+
+
 }
