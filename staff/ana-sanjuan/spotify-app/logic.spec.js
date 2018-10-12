44d7504890ebc30a5f2be0daba354512b9db28f4
adancing <anatsanjuan@gmail.com>
2018-10-12 17:33:28 +0200

    first version spotify-app

---
+++ staff/ana-sanjuan/spotify-app/logic.spec.js
@@ -0,0 +1,27 @@
+describe('logic', () => {
+    describe('search artists', () => {
+        it('should succeed on matching query', () => {
+            var query = 'jackson'
+
+            return logic.searchArtists(query)
+                .then(artists => {
+                    expect(artists).toBeDefined()
+
+                    expect(artists.length).toBeGreaterThan(0)
+                })
+        })
+    })
+
+    describe('list albums', () => {
+        it('should succeed on matching query', () => {
+            var id = '6tbjWDEIzxoDsBA1FuhfPW' // madonna
+
+            return logic.listAlbums(id)
+                .then(albums => {
+                    expect(albums).toBeDefined()
+
+                    expect(albums.length).toBeGreaterThan(0)
+                })
+        })
+    })
+})
