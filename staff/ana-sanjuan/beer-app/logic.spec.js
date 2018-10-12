1218675f7f379f5afa6e50e024279044cb2ff1a8
adancing <anatsanjuan@gmail.com>
2018-10-12 10:21:25 +0200

    add async test

--- staff/ana-sanjuan/beer-app/logic.spec.js
+++ staff/ana-sanjuan/beer-app/logic.spec.js
@@ -1,18 +1,13 @@
 describe('logic', function(){
 
     describe('http', function(){
-        beforeEach(function(done) {
-            setTimeout(function() {
-                done();
-          
-            }, 8000);
-        });
-
-
+        
         it('should succed on retrieveing data from API', function(done){
             var callback = function(beers){
+                
                 expect(beers[0].name).toEqual('Hola Maria');
                 expect(beers[1].name).toEqual('Hola Dali');
+                done();
             };
             errorParam = [];
             query = 'hola';
