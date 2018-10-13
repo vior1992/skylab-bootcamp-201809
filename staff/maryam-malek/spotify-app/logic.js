bd515515855aaaf696bc2b063ed6116f5ee02789
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-13 18:16:24 +0200

    logic done

--- staff/maryam-malek/spotify-app/logic.js
+++ staff/maryam-malek/spotify-app/logic.js
@@ -1,78 +1,6 @@
-var token = 'BQBoRFreFtLBFwBcGavBSFG6LjfaQqPjTLAKMhM-te8TgQwdKZYnTUR1_BALAkJtv0cL7hAW4hUF9gsZllMxgQRNPxd6cQcohVf2ZHAXHvo4wVLzuv-hK43TRZghboW385sPtzPt6_gxJI9KRlXPQKCwpoFkX-pUKw'
 const logic = {
 
-    searchArtists(query) {
-        return new Promise((resolve, reject) => {
-            var xhr = new XMLHttpRequest()
-
-            xhr.addEventListener('load', function () {
-                var res = JSON.parse(xhr.responseText)
-
-                resolve(res.artists.items)
-            })
-
-            xhr.addEventListener('error', function () {
-                reject() // TODO
-            })
-
-            xhr.open('get', 'https://api.spotify.com/v1/search?type=artist&query=' + query)
-
-            // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
-
-            xhr.setRequestHeader('authorization', 'Bearer ' + token)
-
-            xhr.send()
-        })
-    },
-    listAlbums(id){
-        return new Promise((resolve, reject) => {
-            var xhr = new XMLHttpRequest()
-
-            xhr.addEventListener('load', function () {
-                var res = JSON.parse(xhr.responseText)
-
-                resolve(res.items)
-            })
-
-            xhr.addEventListener('error', function () {
-                reject() // TODO
-            })
-
-            xhr.open('get', `https://api.spotify.com/v1/artists/${id}/albums`)
-
-            // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
-
-            xhr.setRequestHeader('authorization', 'Bearer ' + token)
-
-            xhr.send()
-        })
-    },
-
-    listSongs(id){
-        return new Promise((resolve, reject) => {
-            var xhr = new XMLHttpRequest()
-
-            xhr.addEventListener('load', function () {
-                var res = JSON.parse(xhr.responseText)
-
-                resolve(res.items)
-            })
-
-            xhr.addEventListener('error', function () {
-                reject() // TODO
-            })
-
-            xhr.open('get', `https://api.spotify.com/v1/albums/${id}/tracks`)
-
-            // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
-
-            xhr.setRequestHeader('authorization', 'Bearer ' + token)
-
-            xhr.send()
-        })
-    },
-
-    playSong(id) {
+    callApi(path) {
         return new Promise((resolve, reject) => {
             var xhr = new XMLHttpRequest()
 
@@ -86,16 +14,131 @@ const logic = {
                 reject() // TODO
             })
 
-            xhr.open('get', `https://api.spotify.com/v1/audio-features/${id}`)
-           // xhr.open('get', `https://api.spotify.com/v1/tracks/${id}`)
+            xhr.open('get', 'https://api.spotify.com/v1/' + path)
 
-            // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
+            var token = 'BQAM4-okhnzDJ6MZ5I1AbHDG3o-q87j1fXEdQy0_38_CHvBQlbrdhdLuUo502SN3yIvhCsm2TRvo9aOrclj2g1PhrwCg93eOvYRj1RGPdGTPIV0ZBiG4shurIYitJ6E35zExiKXb8IGKpH84Hiwj0dU3kt5gkdIhOA'
 
             xhr.setRequestHeader('authorization', 'Bearer ' + token)
 
             xhr.send()
         })
+
+    },
+    searchArtists(query) {
+        if(typeof query !== 'string' || query.trim().length === 0) throw TypeError (query + ' is not a valid query')
+        
+        return this.callApi(`search?type=artist&query=${query}`)
+    },
+
+    listAlbums(id) {
+        if(typeof id !== 'string' || id.trim().length === 0) throw TypeError (id + ' is not a valid id')
+
+        return this.callApi(`artists/${id}/albums`)
+    },
+
+    listSongs(id){
+        if(typeof id !== 'string' || id.trim().length === 0) throw TypeError (id + ' is not a valid id')
+
+        return this.callApi(`albums/${id}/tracks`)
+         
     }
+    // searchArtists(query) {
+    //     if(typeof query !== 'string' || query.trim().length === 0) throw TypeError (query + ' is not a valid query')
+    //     return new Promise((resolve, reject) => {
+    //         var xhr = new XMLHttpRequest()
+
+    //         xhr.addEventListener('load', function () {
+    //             var res = JSON.parse(xhr.responseText)
+
+    //             resolve(res)
+    //             //.artists.items
+    //         })
+
+    //         xhr.addEventListener('error', function () {
+    //             reject() // TODO
+    //         })
+
+    //         xhr.open('get', 'https://api.spotify.com/v1/search?type=artist&query=' + query)
+
+    //         // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
+
+    //         xhr.setRequestHeader('authorization', 'Bearer ' + token)
+
+    //         xhr.send()
+    //     })
+    // },
+    // listAlbums(id){
+    //     return new Promise((resolve, reject) => {
+    //         var xhr = new XMLHttpRequest()
+
+    //         xhr.addEventListener('load', function () {
+    //             var res = JSON.parse(xhr.responseText)
+
+    //             resolve(res)
+    //         })
+
+    //         xhr.addEventListener('error', function () {
+    //             reject() // TODO
+    //         })
+
+    //         xhr.open('get', `https://api.spotify.com/v1/artists/${id}/albums`)
+
+    //         // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
+
+    //         xhr.setRequestHeader('authorization', 'Bearer ' + token)
+
+    //         xhr.send()
+    //     })
+    // },
+
+    // listSongs(id){
+    //     return new Promise((resolve, reject) => {
+    //         var xhr = new XMLHttpRequest()
+
+    //         xhr.addEventListener('load', function () {
+    //             var res = JSON.parse(xhr.responseText)
+
+    //             resolve(res)
+    //         })
+
+    //         xhr.addEventListener('error', function () {
+    //             reject() // TODO
+    //         })
+
+    //         xhr.open('get', `https://api.spotify.com/v1/albums/${id}/tracks`)
+
+    //         // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
+
+    //         xhr.setRequestHeader('authorization', 'Bearer ' + token)
+
+    //         xhr.send()
+    //     })
+    // },
+}
+    // playSong(id) {
+    //     return new Promise((resolve, reject) => {
+    //         var xhr = new XMLHttpRequest()
+
+    //         xhr.addEventListener('load', function () {
+    //             var res = JSON.parse(xhr.responseText)
+
+    //             resolve(res)
+    //         })
+
+    //         xhr.addEventListener('error', function () {
+    //             reject() // TODO
+    //         })
+
+    //         xhr.open('get', `https://api.spotify.com/v1/audio-features/${id}`)
+    //        // xhr.open('get', `https://api.spotify.com/v1/tracks/${id}`)
+
+    //         // var token = 'BQBPHeoLCmYO9j1rXNoOTA5pEACBfKriJGW8hxquUVW--M7qCSZeK0buIwRBEd9o1iyDdw9rB8K42KitgZId0aeWHXLzHUzmqtvu42qsm66DF8haW--XWsagMLFLK5qrA-Y0w1-1-tMMVQuhN0MZFs2zgWT1EUkBtA'
+
+    //         xhr.setRequestHeader('authorization', 'Bearer ' + token)
+
+    //         xhr.send()
+    //     })
+    // }
     
 
 
@@ -131,4 +174,3 @@ const logic = {
     // listAlbums(id) {
     //     return this.callApi(`artists/${id}/albums`)
     // }
-}
--- staff/maryam-malek/spotify-app/logic.spec.js
+++ staff/maryam-malek/spotify-app/logic.spec.js
@@ -5,23 +5,161 @@ describe('logic', () => {
 
             return logic.searchArtists(query)
                 .then(artists => {
-                    expect(artists).toBeDefined()
+                    expect(artists.artists.items).toBeDefined()
 
-                    expect(artists.length).toBeGreaterThan(0)
+                    expect(artists.artists.items.length).toBeGreaterThan(0)
                 })
         })
+        it('should fail on undefined query', () => {
+            var query = undefined
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        it('should fail on null query', () => {
+            var query = null
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        it('should fail on blank query', () => {
+            var query = '   '
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        it('should fail on empty query', () => {
+            var query = ''
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        it('should fail on non-string query (number)', () => {
+            var query = 1
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        it('should fail on non-string query (boolean)', () => {
+            var query = true
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        it('should fail on non-string query (object)', () => {
+            var query = {}
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        it('should fail on non-string query (array)', () => {
+            var query = []
+
+            expect(() => {
+                logic.searchArtists(query)
+
+            }).toThrowError(TypeError, query + ' is not a valid query')
+        })
+        // it('should fail on unmatching query', function () {
+        //     var query = 'lalalalala'
+
+        //     return logic.searchArtists(query)
+        //         .catch(artists => {
+        //             expect(artist).toBeDefined();
+
+        //             expect(artists.total).toEqual(10);
+        //         });
+        // });
     })
 
     describe('list albums', () => {
-        it('should succeed on matching query', () => {
+        it('should succeed on matching id', () => {
             var id = '6tbjWDEIzxoDsBA1FuhfPW' // madonna
 
             return logic.listAlbums(id)
                 .then(albums => {
-                    expect(albums).toBeDefined()
+                    expect(albums.items).toBeDefined()
 
-                    expect(albums.length).toBeGreaterThan(0)
+                    expect(albums.items.length).toBeGreaterThan(0)
                 })
+        })
+        it('should fail on undefined id', () => {
+            var id = undefined
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on null id', () => {
+            var id = null
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on blank id', () => {
+            var id = '   '
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on empty id', () => {
+            var id = ''
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (number)', () => {
+            var id = 1
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (boolean)', () => {
+            var id = true
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (object)', () => {
+            var id = {}
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (array)', () => {
+            var id = []
+
+            expect(() => {
+                logic.listAlbums(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
         })
     })
 
@@ -31,23 +169,76 @@ describe('logic', () => {
 
             return logic.listSongs(id)
                 .then(songs => {
-                    expect(songs).toBeDefined()
+                    expect(songs.items).toBeDefined()
 
-                    expect(songs.length).toBeGreaterThan(0)
+                    expect(songs.items.length).toBeGreaterThan(0)
                 })
+        })
+        it('should fail on undefined id', () => {
+            var id = undefined
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on null id', () => {
+            var id = null
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on blank id', () => {
+            var id = '   '
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on empty id', () => {
+            var id = ''
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (number)', () => {
+            var id = 1
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (boolean)', () => {
+            var id = true
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (object)', () => {
+            var id = {}
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
+        })
+        it('should fail on non-string id (array)', () => {
+            var id = []
+
+            expect(() => {
+                logic.listSongs(id)
+
+            }).toThrowError(TypeError, id + ' is not a valid id')
         })
     })
 
-    describe('play song', () => {
-        it('should succeed on matching query', () => {
-            var id = '2S9zg7NJN8Pk8nI0t06HxT' //Cat Empire Song
-
-            return logic.playSong(id)
-                .then(song => {
-                    expect(song).toBeDefined()
-                    // Falta alguna comprovació
-                })
-        })
-    })
 
 })
