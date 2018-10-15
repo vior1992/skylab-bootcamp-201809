292c2921f51365398e90e91f888beca3cd1c728f
Fede <fedegomezcucchiararo@gmail.com>
2018-10-15 17:32:46 +0200

    initial feature/react-calculator

---
+++ staff/fede-gomez/react-calculator/main.jsx
@@ -0,0 +1,16 @@
+const root = document.getElementById('root')
+
+class plusButton extends React.Component {
+
+    whenClicked = (num1, num2) => {
+        return 
+    }
+
+    render() {
+        return <button onClick={this.whenClicked}>{this.state.status}</button>
+    }
+}
+
+ReactDOM.render(<App />, root)
+
+
