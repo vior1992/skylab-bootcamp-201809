812995c9eafbf1c313b45ecf2bb8d9f5d19aec6d
Carlos Diestro <cardiest@gmail.com>
2018-10-15 23:29:46 +0200

    add postit-app

---
+++ staff/carlos-diestro/postit-app/main.jsx
@@ -0,0 +1,58 @@
+class PostItApp extends React.Component {
+  state = {
+    items: [],
+    text: ''
+  }
+
+  render() {
+    return (
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <h1>Post-it App</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <textarea className="form-control" rows="3" placeholder="Write something..." onChange={this.handleChange}></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary mb-2">Create a Post-it</button>
+          </form>
+          <PostItList items={this.state.items} />
+        </div>
+      </div>
+    )
+  }
+
+  handleChange = event => {
+    this.setState({ text: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    if (!this.state.text.length) {
+      return
+    }
+
+    const newItem = {
+      text: this.state.text
+    }
+
+    this.setState(state => ({
+      item: state.items.push(newItem),
+      text: ''
+    }))
+  }
+}
+
+class PostItList extends React.Component {
+  render() {
+    return (
+      <div className="post-it">
+        {this.props.items.forEach(item => (
+          <div className="alert alert-warning" role="alert">{item.text}</div>
+        ))}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<PostItApp />, document.getElementById('root'));
