04078514e2e2fb73d6e5172a575e442bffe6fdff
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-15 18:57:36 +0200

    update

--- staff/gianluca-bragaglia/react-calculator/main.jsx
+++ staff/gianluca-bragaglia/react-calculator/main.jsx
@@ -1,52 +1,74 @@
-const root = document.getElementById('root')
-
-
-class InputFirstNumber extends React.Component {
-
-    render() {
-        return <input type="number"/>
-    }
-}
-
-class ButtonSum extends React.Component {
+function Button(props) {
+    console.log(props);
     
-
-    sum = () => {
-
-    }
-
-    render() {
-        return <button onClick={this.sum}>+</button>
-    }
-}
-
-class ButtonMultiply extends React.Component {
-    
-
-    multiply = () => {
-
-    }
-
-    render() {
-        return <button onClick={this.multiply}>*</button>
-    }
+    return <button className="btn-primary" onClick={props.whenClick}>{props.operation}</button>
 }
 
 class App extends React.Component {
+    state = { numberA: '', numberB: '', result: '' }
 
+    keepNumberA = event => {
+        const numberA = event.target.value
+
+        this.setState({ numberA })
+    }
+
+    keepNumberB = event => {
+        const numberB = event.target.value
+
+        this.setState({ numberB })
+    }
+
+    add = () => {
+        const numberA = parseFloat(this.state.numberA)
+        const numberB = parseFloat(this.state.numberB)
+
+        const result = numberA + numberB
+
+        this.setState({ result })
+    }
+
+    subtract = () => {
+        const numberA = parseFloat(this.state.numberA)
+        const numberB = parseFloat(this.state.numberB)
+
+        const result = numberA - numberB
+
+        this.setState({ result })
+    }
+
+    multiply = () => {
+        const numberA = parseFloat(this.state.numberA)
+        const numberB = parseFloat(this.state.numberB)
+
+        const result = numberA * numberB
+
+        this.setState({ result })
+    }
+
+    divide = () => {
+        const numberA = parseFloat(this.state.numberA)
+        const numberB = parseFloat(this.state.numberB)
+
+        const result = numberA / numberB
+
+        this.setState({ result })
+    }
 
     render() {
-        return  <section>
-                 <InputFirstNumber />
-                 <ButtonSum />
-                 <ButtonMultiply />
-                </section>
+        return <div className="container">
+            <input value={this.state.numberA} type="number" onChange={this.keepNumberA} />
 
+            <Button operation="+" whenClick={this.add}></Button>
+            <Button operation="-" whenClick={this.subtract}></Button>
+            <Button operation="*" whenClick={this.multiply}></Button>
+            <Button operation="/" whenClick={this.divide}></Button>
+
+            <input value={this.state.numberB} type="number" onChange={this.keepNumberB} />
+
+            =<input value={this.state.result} type="result" disabled />
+        </div>
     }
-        
-   
 }
 
-
-
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, document.getElementById('root'))
