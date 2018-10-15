f88742774bfc59a340c1bd674281081a2bf539b2
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-10-15 10:01:49 +0200

    fixed logic

--- staff/joel-molero/spotify-app/logic.js
+++ staff/joel-molero/spotify-app/logic.js
@@ -1,3 +1,5 @@
+var token = 'BQC8POiGlCFCG2eQbT8dB3EHYhMSb-D_Ia5UmkWmgstv6qeR-smwFKvhnJEm8zZH_orkn-dEhN8_cSdf0hBFOV2UgZjCai7n73TcVZuO76Skri1tgaOlSSHdMeGhkTIWTsL7Mqnxdata'
+
 const logic = {
     searchArtists(query) {
         return new Promise((resolve, reject) => {
@@ -15,8 +17,6 @@ const logic = {
 
             xhr.open('get', 'https://api.spotify.com/v1/search?type=artist&query=' + query)
 
-            var token = 'BQC8POiGlCFCG2eQbT8dB3EHYhMSb-D_Ia5UmkWmgstv6qeR-smwFKvhnJEm8zZH_orkn-dEhN8_cSdf0hBFOV2UgZjCai7n73TcVZuO76Skri1tgaOlSSHdMeGhkTIWTsL7Mqnxdata'
-
             xhr.setRequestHeader('authorization', 'Bearer ' + token)
 
             xhr.send()
@@ -29,16 +29,34 @@ const logic = {
             xhr.addEventListener('load', function () {
                 var res = JSON.parse(xhr.responseText)
 
-                resolve(res.artists.items)
+                resolve(res.items)
             })
 
             xhr.addEventListener('error', function () {
                 reject() // TODO
             })
 
-            xhr.open('get', 'https://api.spotify.com/v1/search?type=artist&query=' + query)
+            xhr.open('get', 'https://api.spotify.com/v1/albums/' + id)
 
-            var token = 'BQC8POiGlCFCG2eQbT8dB3EHYhMSb-D_Ia5UmkWmgstv6qeR-smwFKvhnJEm8zZH_orkn-dEhN8_cSdf0hBFOV2UgZjCai7n73TcVZuO76Skri1tgaOlSSHdMeGhkTIWTsL7Mqnxdata'
+            xhr.setRequestHeader('authorization', 'Bearer ' + token)
+
+            xhr.send()     
+    }
+
+    listTracks(id) {
+        var xhr = new XMLHttpRequest()
+
+            xhr.addEventListener('load', function () {
+                var res = JSON.parse(xhr.responseText)
+
+                resolve(res.items)
+            })
+
+            xhr.addEventListener('error', function () {
+                reject() // TODO
+            })
+
+            xhr.open('get', 'https://api.spotify.com/v1/albums/' + id + '/tracks')
 
             xhr.setRequestHeader('authorization', 'Bearer ' + token)
 
