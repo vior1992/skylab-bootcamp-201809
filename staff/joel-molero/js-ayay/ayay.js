a8d649eff4b393d2a12a2c219f4e72a9d46f70e9
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-10-15 10:16:46 +0200

    added functions

--- staff/joel-molero/js-ayay/ayay.js
+++ staff/joel-molero/js-ayay/ayay.js
@@ -14,32 +14,53 @@ Ayay.prototype.push = function(element) {
     this.length++;
 };
 
+
 Ayay.prototype.pop = function() {
-    var arr;
-    
+    var result = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+    return result;
 };
 
-Ayay.prototype.forEach = function(callback) {
-    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
-    for (var i = 0; i < this.length; i++) callback(this[i], i, this);
+ Ayay.prototype.forEach = function(callback) {
+
+    if (typeof callback !== 'function') throw new TypeError('callback is not a function');
+
+     for (var i = 0; i < this.length; i++) callback(this[i], i, this);
+
 };
 
-Ayay.prototype.map = function(callback) {
-    //var arr;
-    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
 
-    for (var x = 0; x < this.length; x++) arr[x] = callback[x];
+ Ayay.prototype.map = function(callback) {
+
+    if (typeof callback !== 'function') throw new TypeError('callback is not a function');
+     var result = new Ayay();
+
+    for (var i = 0; i < this.length; i++) {
+        result[i] = callback(this[i], i, this);
+        result.length++;
+    }
+
+    return result;
 };
 
-Ayay.prototype.sort = function() {
-    
-};
 
-Ayay.prototype.filter = function(callback) {
+ Ayay.prototype.sort = function() {
     // TODO
 };
 
-Ayay.prototype.find = function(callback) {
+
+ Ayay.prototype.filter = function(callback) {
+    if (typeof callback !== 'function') throw new TypeError(callback + 'is not a function')
+         var newArr = new Ayay()
+         for (let i=0; i<this.length; i++) {
+            if (callback(this[i])) newArr.push(this[i])
+        }
+         return newArr
+};
+
+
+ Ayay.prototype.find = function(callback) {
     // TODO
 };
