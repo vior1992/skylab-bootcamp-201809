38cba7fe00e954a1c1f40a99e6cf8f0ac547666d
Almu Iturgoyen <almudena.iturgoyen@gmail.com>
2018-10-16 16:32:02 +0200

    new changes postit

--- staff/almu-itur/postit-app/main.jsx
+++ staff/almu-itur/postit-app/main.jsx
@@ -1,39 +1,64 @@
-function PostIt(props) {
-    return <section><article>{props.note}</article></section>
-}
+class InputForm extends React.Component {
+    state = { text: '' }
 
-class App extends React.Component {
+    handleInput = event => {
+        console.log('InputForm', 'handleInput (setState)')
+        
+        const text = event.target.value
 
-    state = { note: '', notes: [] }
-
-    keepNote = event => { 
-        var note = event.target.value
-        this.setState({ note })
+        this.setState({ text })
     }
 
-    createNote = event => {
-        event.preventDefault()     
-        this.setState(prevState => ({
-            notes: [...prevState.notes, this.state.note]
-        })) 
+    handleSubmit = event => {
+        console.log('InputForm', 'handleSubmit (setState)')
+
+        event.preventDefault()
+
+        this.props.onSubmit(this.state.text)
+
+        this.setState({ text: '' })
     }
 
     render() {
-        return (
-            <div className="container">
-                <form>
-                    <label>Add text</label>
-                    <input type="text" onChange={this.keepNote}/>
-                    <input type="submit" onClick={this.createNote} value="Create note"/>
-                </form>
-                <div className="postIt-container">
-                    {this.state.notes.map((post) => {
-                        return <PostIt key={post} note={post} />
-                    })}
-                </div>
+        console.log('InputForm', 'render')
 
-            </div>
-        )
+        return <form onSubmit={this.handleSubmit}>
+            <textarea value={this.state.text} placeholder="Write text here..." onChange={this.handleInput} />
+            <button type="submit">Create</button>
+        </form>
+    }
+}
+
+function Post(props) {
+    console.log('Post', '"render"')
+
+    return <article className="post">{props.text}</article>
+}
+
+class App extends React.Component {
+    state = { posts: [] }
+
+    handleSubmit = text => {
+        console.log('App', 'handleSubmit (setState)')
+
+        const posts = this.state.posts.concat(text)
+
+        this.setState({ posts })
+    }
+
+    render() {
+        console.log('App', 'render')
+
+        return <div>
+            <h1>Post-It App</h1>
+
+            <InputForm onSubmit={this.handleSubmit} />
+
+            <section>
+                {/* {this.state.posts.map((post, index) => <article key={index} className="post">{post}</article>)} */}
+                {this.state.posts.map((post, index) => <Post key={index} text={post} />)}
+            </section>
+        </div>
     }
 }
 
