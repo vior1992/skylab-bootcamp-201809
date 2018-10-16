8e07f73f99d04470a43a78950cc79c78952ad42c
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-16 16:29:48 +0200

    update

--- staff/gianluca-bragaglia/postit-app/main.jsx
+++ staff/gianluca-bragaglia/postit-app/main.jsx
@@ -3,7 +3,7 @@ function PostIt(props) {
     
     return <section>
                 <article className="article">{props.text}</article>
-                <button onClick={() => props.delete(props.index)}>delete</button>
+                <button className="button-delete" onClick={() => props.onDelete(props.index)}>delete</button>
             </section>        
 } 
 
@@ -30,18 +30,12 @@ class App extends React.Component {
 
     }
 
-    /* deletePostit = index => {
-        const data = this.state.texts
-        this.setState({ 
-        texts: [...data.slice(0,index), ...data.slice(index+1)]
-        })
-    } */
 
-    deletePostit = index => {
-      //  console.log(index);
-        
-        this.state.texts.splice(index,1)
-        this.setState({texts:this.state.texts})
+    handleDeletePostit = index => {       
+        /* this.state.texts.splice(index,1)
+        this.setState({texts:this.state.texts}) */
+        const texts = this.state.texts.filter((texts, _index) => index !== _index)
+        this.setState({texts})
     }
 
     render() {
@@ -49,11 +43,11 @@ class App extends React.Component {
             <h1>Post-It App</h1>
             <form onSubmit={this.handleSubmit}>
                 <textarea placeholder="Write text here..." type="text" onChange={this.handleChange} value={this.state.text} /><br></br>
-                <button type="submit">Create</button>
+                <button className="button" type="submit">Create</button>
             </form>
             <div className="posts-container">
                 {this.state.texts.map((post,index) => {
-                    return <PostIt delete={this.deletePostit} key={index} index ={index} text={post} />
+                    return <PostIt onDelete={this.handleDeletePostit} key={index} index ={index} text={post} />
                         
                 })}
             </div>
