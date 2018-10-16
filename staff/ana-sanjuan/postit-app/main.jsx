eb5698f2bbcefcf7069648106bff9ab6f2d42ba9
adancing <anatsanjuan@gmail.com>
2018-10-16 11:10:34 +0200

    add all

--- staff/ana-sanjuan/postit-app/main.jsx
+++ staff/ana-sanjuan/postit-app/main.jsx
@@ -1,17 +1,37 @@
-function App() {
-
-    return (
-    <div>
-        <form>
-            <textarea placeholder="Write text here..."></textarea>
-            <button type="submit">Create</button>
-        </form>
-
-        <section>
-            <article style="background-color: yellow; width: 100px; height: 100px">My first postit</article>
+function Article (props) {
+    return <section>
+        <article>{props.postItText}</article>
         </section>
-    </div>
-    )
 }
 
-ReactDOM.render(<App />, root) 
+class App extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = { textValue: "", posts: []}
+    }
+    
+    keepValue = event => {
+        const textValue = event.target.value
+        this.setState({textValue})
+    }
+
+    createPostIt = event => {
+        event.preventDefault()
+        this.setState(prevState => ({
+            posts: [...prevState.posts, this.state.textValue]
+          }))    
+     }
+
+    render() {
+        return <section> 
+            <form>
+                <textarea placeholder="Write text here..." onChange={this.keepValue} value = {this.state.textValue}></textarea>
+                <button type= "button" onClick = {this.createPostIt}>Create</button>
+            </form>
+                {this.state.posts.map(post => <Article postItText= {post}/>)}
+        </section>
+    }
+    
+}
+
+ReactDOM.render(<App />, root)
