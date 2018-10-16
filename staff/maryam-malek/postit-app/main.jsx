f46b80bd466ce284a836a2456db78c44cbac6a6c
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-16 14:05:56 +0200

    delete done

--- staff/maryam-malek/postit-app/main.jsx
+++ staff/maryam-malek/postit-app/main.jsx
@@ -1,39 +1,65 @@
-function PostIt (props) {
+class InputForm extends React.Component {
+    state = { text: '' }
 
-    return <section>
-        <article className='postIt'>{props.paint}</article>
-    </section> 
+    handleInput = event => {
+        const text = event.target.value
+
+        this.setState({ text })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        this.props.onSubmit(this.state.text)
+
+        this.setState({ text: '' })
+    }
+
+    render() {
+        return <form onSubmit={this.handleSubmit}>
+            <textarea placeholder='Write your text...' value={this.state.text} onChange={this.handleInput}></textarea>
+            <button type='submit'>Create</button>
+        </form>
+    }
+}
+
+function PostIt(props) {
+    return <div className='postIt'>
+        <article >{props.paint}</article>
+        <button className='postIt__button' type='button' onClick={() => {props.onClick(props.index)}} >X</button>
+    </div>
 }
 
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { inputText: [], text: '' }
+        this.state = { inputText: [] }
     }
 
-    show = event => {
-        event.preventDefault()     
+    handleSubmit = text => {
         this.setState(prevState => ({
-            inputText: [...prevState.inputText, this.state.text]
-          }))    
+            inputText: [...prevState.inputText, text]
+        }))
     }
 
-    keepText = event => {
-        const text = event.target.value
-        this.setState({text})
-   
+    handleClick = key => {
+        // const inputText = this.state.inputText.splice(this.state.inputText[key])
+        const newArr1 = this.state.inputText.slice(0, key)
+        const newArr2 = this.state.inputText.slice(key+1)
+        const inputText = newArr1.concat(newArr2)
+        this.setState({inputText})
     }
 
     render() {
         return <section>
             <h1>Post-It App</h1>
-            <form>
-                <textarea placeholder='Write your text...' value={this.state.text} onChange={this.keepText}></textarea>
-                <button type= 'button' onClick={this.show}>Create</button>
-            </form>
-            {this.state.inputText.map(x => <PostIt paint={x}/>)}
-        </section >
 
+            <InputForm onSubmit={this.handleSubmit} />
+
+            <section>
+                {this.state.inputText.map((x, index) => <PostIt paint={x} key={x} index={index} onClick={index => this.handleClick(index)}/>)}
+            </section>
+        </section >
     }
 }
 
