39034b1f70071dd47d2386fdffeca943d402be74
Carlos Diestro <cardiest@gmail.com>
2018-10-16 11:32:03 +0200

    fix errors and end postit app

--- staff/carlos-diestro/postit-app/main.jsx
+++ staff/carlos-diestro/postit-app/main.jsx
@@ -11,11 +11,11 @@ class PostItApp extends React.Component {
           <h1>Post-it App</h1>
           <form onSubmit={this.handleSubmit}>
             <div className="form-group">
-              <textarea className="form-control" rows="3" placeholder="Write something..." onChange={this.handleChange}>{this.state.text}</textarea>
+              <textarea className="form-control" rows="3" autoFocus placeholder="Write something..." onChange={this.handleChange} value={this.state.text}></textarea>
             </div>
             <button type="submit" className="btn btn-primary mb-2">Create a Post-it</button>
           </form>
-          <PostItList items={this.state.items} />
+          <PostItList items={this.state.items} onRemoveItem={this.handleRemoveItem} />
         </div>
       </div>
     )
@@ -37,22 +37,33 @@ class PostItApp extends React.Component {
     }
 
     this.setState(state => ({
-      items: state.items.concat(newItem),
+      items: [...state.items, newItem],
       text: ''
     }))
+
+    event.currentTarget[0].focus()
+  }
+
+  handleRemoveItem = index => {
+    this.setState({
+      items: this.state.items.filter((item, itemIndex) => itemIndex !== index)
+    })
   }
 }
 
-class PostItList extends React.Component {
-  render() {
-    return (
-      <div className="post-it">
-        {this.props.items.map(item => (
-          <div className="alert alert-warning" role="alert">{item.text}</div>
-        ))}
-      </div>
-    )
-  }
+const PostItList = props => {
+  return (
+    <div className="post-it">
+      {props.items.map((item, index) => (
+        <div className="alert alert-warning alert-dismissible fade show" key={index}>
+          {item.text}
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => props.onRemoveItem(index)}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 ReactDOM.render(<PostItApp />, document.getElementById('root'));
