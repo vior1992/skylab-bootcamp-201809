60a1c715238f4a499473294a049b68d4fdc1c348
adancing <anatsanjuan@gmail.com>
2018-10-16 19:09:00 +0200

    add layers

--- staff/ana-sanjuan/postit-app/main.jsx
+++ staff/ana-sanjuan/postit-app/main.jsx
@@ -1,39 +1,67 @@
-class Article extends React.Component {
-    state = {}    
+const storage = sessionStorage
+// const storage = localStorage
 
-    handleClick = event =>{
-        event.preventDefault()
 
-        this.props.DadDelete(this.props.index)
+//Model (domain)
 
-    }
-    
-    render(){
-        return <article className="post">
-        <p>{this.props.postItText}</p>
-        <button className="btn btn-warning" onClick = {this.handleClick}>Delete Me</button>
-        </article>
+if (!storage.getItem('postits'))
+    storage.setItem('postits', JSON.stringify([]))
+
+
+class Postit {
+    constructor(text) {
+        this.text = text
+        this.id = Date.now()
     }
 }
 
-class InputForm extends React.Component{
+//bussiness
+
+const logic = {
+    
+    createPostit(text) {
+        const postit = new Postit(text)
+
+        const postits = JSON.parse(storage.getItem('postits'))
+
+        postits.push(postit)
+
+        storage.setItem('postits', JSON.stringify(postits))
+
+    },
+
+    listPostits(id) {
+        return JSON.parse(storage.getItem('postits'))
+
+    },
+
+    deletePostit(id){
+        let postits = JSON.parse(storage.getItem('postits'))
+
+        postits = postits.filter(postit => postit.id !== id)
+
+        storage.setItem('postits', JSON.stringify(postits))
+    }
+}
+
+
+class InputForm extends React.Component {
     state = {textValue: "" }
 
     handleInput = event => {
         const textValue = event.target.value
 
         this.setState({textValue})
-
     }
 
     handleSubmit = event => {
         event.preventDefault()
-
+        
         this.props.DadSubmit(this.state.textValue)
 
         this.setState({ textValue: '' })
       
-     }
+    }
 
     render(){
         return<form onSubmit={this.handleSubmit} >
@@ -43,26 +71,54 @@ class InputForm extends React.Component{
     }
 }
 
-class App extends React.Component{
-    state = { posts: []}
+function Article (props) {
+
+    return <article className="post">
+        <p>{props.postItText}</p>
+        <button className="btn btn-warning" onClick = {()=> props.DadDelete(props.id)}>Delete Me</button>
+        <button className="btn btn-warning" onClick = {()=> props.DadEdit(props.id)}>Edit Me</button>
+        <InputForm />
+       
+        {/* <button className="btn btn-warning" onClick = {()=> props.DadEdit(props.id)}>Edit Me</button> */}
+        
+     </article>
+}
+
+class App extends React.Component {
+    state = { postits: JSON.parse(storage.getItem('postits'))}
 
     handleSubmit = text => {
-        const posts = [...this.state.posts, text]
+        logic.createPostit(text)
 
-        this.setState({ posts })
+        this.setState({ postits: logic.listPostits() })
     }
 
-    handleDelete = index => {
-        this.state.posts.splice(index,1)
-    
-        this.setState({posts: this.state.posts})
+    handleDelete = id => {
+        
+        logic.deletePostit(id)
+
+        this.setState({ postits: logic.listPostits() })
     }
+
+    handleEdit = (newText, id) => {
+
+        let postits = JSON.parse(storage.getItem('postits'))
+        
+        let editPostit = postits.filter(postit => postit.id === id)
+
+        editPostit.text = newText
+        
+        storage.setItem('postits', JSON.stringify(postits))
+    }
+
+
     render() {
         return <section> 
+            <h1>Post-It App <i className="fas fa-sticky-note"></i></h1>
             <InputForm DadSubmit={this.handleSubmit}/>
-                
-            {this.state.posts.map((post, index) => <Article key={index} index={index} postItText={post} DadDelete={this.handleDelete}/>)}
-            
+            <section>  
+                {this.state.postits.map(postit => <Article key={postit.id} id={postit.id} postItText={postit.text} DadDelete={this.handleDelete} DadEdit={this.handleEdit}/>)}
+            </section> 
         </section>
     }
 }
