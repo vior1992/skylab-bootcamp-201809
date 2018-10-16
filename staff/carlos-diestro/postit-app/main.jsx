f3d88057207d5e93753b77597308c67892feadf9
Carlos Diestro <cardiest@gmail.com>
2018-10-16 15:16:18 +0200

    add form component

--- staff/carlos-diestro/postit-app/main.jsx
+++ staff/carlos-diestro/postit-app/main.jsx
@@ -1,7 +1,6 @@
 class PostItApp extends React.Component {
   state = {
-    items: [],
-    text: ''
+    items: []
   }
 
   render() {
@@ -9,39 +8,17 @@ class PostItApp extends React.Component {
       <div className="row justify-content-center">
         <div className="col-6">
           <h1>Post-it App</h1>
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <textarea className="form-control" rows="3" autoFocus placeholder="Write something..." onChange={this.handleChange} value={this.state.text}></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary mb-2">Create a Post-it</button>
-          </form>
+          <Form onSubmit={this.handleSubmit} />
           <PostItList items={this.state.items} onRemoveItem={this.handleRemoveItem} />
         </div>
       </div>
     )
   }
 
-  handleChange = event => {
-    this.setState({ text: event.target.value })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-
-    if (!this.state.text.length) {
-      return
-    }
-
-    const newItem = {
-      text: this.state.text
-    }
-
-    this.setState(state => ({
-      items: [...state.items, newItem],
-      text: ''
-    }))
-
-    event.currentTarget[0].focus()
+  handleSubmit = text => {
+    this.setState({
+      items: [...this.state.items, text]
+    })
   }
 
   handleRemoveItem = index => {
@@ -56,6 +33,41 @@ class PostItApp extends React.Component {
   // }
 }
 
+class Form extends React.Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = event => {
+    this.setState({ text: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    if (!this.state.text.length) {
+      return
+    }
+
+    this.props.onSubmit(this.state.text)
+
+    this.setState({ text: '' })
+
+    event.currentTarget[0].focus()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <textarea className="form-control" rows="3" autoFocus placeholder="Write something..." onChange={this.handleChange} value={this.state.text}></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary mb-2">Create a Post-it</button>
+      </form>
+    )
+  }
+}
+
 const PostItList = props => {
   return (
     <div className="post-it my-3">
@@ -63,7 +75,7 @@ const PostItList = props => {
         {props.items.map((item, index) => (
           <div className="col-sm-12 col-md-6">
             <div className="alert alert-warning alert-dismissible fade show" key={index}>
-              {item.text}
+              {item}
               {/* Send event 
               <button data-id={index} type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={props.onRemoveItem}></button> */}
               <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => props.onRemoveItem(index)}>
