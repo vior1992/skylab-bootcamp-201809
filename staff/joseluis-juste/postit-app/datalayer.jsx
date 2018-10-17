0b8c75addda568a44a5616924926865abdf24ff2
scrafy <scrafy26@gmail.com>
2018-10-17 10:39:09 +0200

    modal

---
+++ staff/joseluis-juste/postit-app/datalayer.jsx
@@ -0,0 +1,9 @@
+class PostIt{
+
+    constructor(_text){
+
+        this.text = _text;
+        this.id = Date.now();
+
+    }
+}
--- staff/joseluis-juste/postit-app/main.jsx
+++ staff/joseluis-juste/postit-app/main.jsx
@@ -1,60 +1,112 @@
-class PostIt extends React.Component{
+class PostItComponent extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-       
+
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
-        var pseudoBefore = window.getComputedStyle(this._reactInternalFiber.return.stateNode.getElementsByTagName("article")[0], ':before') 
-        
+        var pseudoBefore = window.getComputedStyle(this._reactInternalFiber.return.stateNode.getElementsByTagName("article")[0], ':before')
+
     }
 
-    render(){        
-        return <article onClick={() => {this.props.onDeletePost(this.props.index)}} className="postit icon-x">{this.props.message}</article>    
+    showModal = () => {
+
+        $('#myModalHorizontal').modal('show');
+
+    }
+
+    render() {
+        return <article onClick={() => { /*this.props.onDeletePost(this.props.index) }*/ }} className="postit icon-x">{this.props.message}
+            <button onClick={(ev) => this.showModal()} className="btn btn-primary btn-lg">Edit</button>
+        </article>
     }
 }
 
-class BoardComponent extends React.Component{
+class BoardComponent extends React.Component {
 
-    state = {posts:[]}
+    state = { posts: [] }
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
     addPostIt = (message) => {
 
-        this.state.posts.push(message);
-        this.setState({posts:this.state.posts});
+        this.state.posts.push(new PostIt(message));
+        const postss =
+            this.setState({ posts: this.state.posts });
     }
 
-    deletePost = (index) =>{
+    deletePostIt = (index) => {
 
-        this.state.posts.splice(index,1);
-        this.setState({posts:this.state.posts});
+        let filtered = this.state.posts.filter((post) => post.id !== index);
+        this.setState({ posts: filtered });
     }
- 
 
-    render(){
+    editPostIt = (index) => {
 
-        
+        let filtered = this.state.posts.filter((post) => post.id !== index);
+        this.setState({ posts: filtered });
+    }
+
+
+
+    render() {
+
+
         return <section className="board">
-
+            <div className="modal fade" id="myModalHorizontal" tabIndex={-1} role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        {/* Modal Body */}
+                        <div className="modal-body">
+                            <form className="form-horizontal" role="form">
+                                <div className="form-group">
+                                    <div className="col-sm-10">
+                                        <input type="text" className="form-control" id="inputEmail3" placeholder="Text..." />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <div className="col-sm-offset-2 col-sm-10">
+                                        <button type="submit" className="btn btn-default">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        {/* Modal Footer */}
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">
+                                Close
+                </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <form action="">
-                <input type="text" name="message" id="message" placeholder = "Type your postit message..."/>
-                <button onClick = {(ev) => {ev.preventDefault();this.addPostIt(document.getElementById("message").value)}}>Send...</button>
+                <input type="text" name="message" id="message" placeholder="Type your postit message..." />
+                <button onClick={(ev) => { ev.preventDefault(); this.addPostIt(document.getElementById("message").value) }}>Send...</button>
             </form>
             <section className="board__panel-postits">
                 {
-                    this.state.posts.map((post,index) => <PostIt onDeletePost = {this.deletePost}  index = {index} message = {post}></PostIt>)                 
+                    this.state.posts.map((post, index) => <PostItComponent key={post.id} index={post.id} onDeletePost={this.deletePost} message={post.text}></PostItComponent>)
                 }
             </section>
 
-        </section>       
+        </section>
 
     }
 }
 
-ReactDOM.render(<BoardComponent />, document.getElementById('root'));
+class App extends React.Component {
+
+    render() {
+
+        return <BoardComponent></BoardComponent>
+    }
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
