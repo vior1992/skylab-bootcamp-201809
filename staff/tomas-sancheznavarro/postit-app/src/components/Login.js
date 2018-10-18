09f0546c74597d8ca0e8b8a5b94a9cb7807a685f
Tomás Sánchez Navarro <tsanchez.navarro@gmail.com>
2018-10-18 15:19:38 +0200

    updated code

--- staff/tomas-sancheznavarro/postit-app/src/components/Login.js
+++ staff/tomas-sancheznavarro/postit-app/src/components/Login.js
@@ -3,10 +3,30 @@ import React, {Component} from 'react'
 class Login extends Component {
     state = { username: '', password: '' }
 
+    handleUsernameChange = event => {
+        const username = event.target.value
+
+        this.setState({ username })
+    }
+
+    handlePasswordChange = event => {
+        const password = event.target.value
+
+        this.setState({ password })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        const { username, password } = this.state
+
+        this.props.onLogin(username, password)
+    }
+
     render() {
-        return <form>
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Password" />
+        return <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Username" onChange={this.handleUsernameChange} />
+            <input type="password" placeholder="Password" onChange={this.handlePasswordChange} />
             <button type="submit">Login</button>
         </form>
     }
