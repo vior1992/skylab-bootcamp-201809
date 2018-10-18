2cdd3a9f09cabe7c3779e7a4b9adf92a1da63b5d
scrafy <scrafy26@gmail.com>
2018-10-18 13:49:25 +0200

    react-app

---
+++ staff/joseluis-juste/postit-app/src/custom-components/board.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import logic from '../services/logic'
+import LoginModalComponent from './loginmodal'
+import RegisterModalComponent from './registermodal'
+import ModalComponent from './modal'
+import PostItComponent from './postit'
+import $ from 'jquery'
+
+
+
+class BoardComponent extends React.Component {
+
+    state = {errorLoginMessage:"", logged:false, actual_user:{} }
+
+    addPostIt = (message) => {
+
+        this.setState({actual_user:  logic.createPostit(this.state.actual_user.id, message)})
+       
+    }
+
+    deletePostIt = (id) => {
+
+         this.setState({actual_user:  logic.deletePostit(this.state.actual_user.id, id)})
+    }
+
+    editPostIt = (id, text) => {
+
+        this.setState({actual_user:  logic.updatePostit(this.state.actual_user.id, id, text)})
+    }
+
+    showModalRegister = () => {
+
+        $('#register-modal').modal('show') //not use Jquery
+
+    }
+
+    showModalLogin = () => {
+
+        $('#login-modal').modal('show') //not use Jquery
+
+    }
+
+    loginUser  = (username, password) => {
+
+        const user = logic.loginUser(username, password)
+        if (user){
+            this.setState({logged:true, actual_user: user})
+
+        }else{
+            this.setState({errorLoginMessage: "The credentials entered are not correct..."})
+            $('#modal').modal('show')
+        }
+
+    }
+
+    registerUser = (name, surname, username, password) => {
+        
+        const user = logic.registerUser(name, surname, username, password)
+        this.setState({actual_user:user})
+        
+    }
+    
+    logoutUser = () => {
+
+        this.setState({logged:false, actual_user:{}})
+    }
+
+    render() {
+        const {logged, errorLoginMessage, actual_user} = this.state
+
+        return <section className="board">
+            <RegisterModalComponent key="registerModal" onRegisterUser = {this.registerUser}></RegisterModalComponent>
+            <LoginModalComponent key="loginModal" onLoginUser = {this.loginUser}></LoginModalComponent>
+             <ModalComponent key="modal" title ="Error Login" message = {errorLoginMessage}/>
+            <section className="board__forms">
+                {logged && <form action="">
+                    <input type="text" name="message" id="message" placeholder="Type your postit message..." />
+                    <button className="button" onClick={(ev) => { ev.preventDefault(); this.addPostIt(document.getElementById("message").value) }}>Send...</button>
+                </form>}
+                <section className="board__panel-buttons">
+                    {!logged  && <button onClick={() => this.showModalRegister()} className="button">Register</button>}
+                    {!logged  && <button onClick={() => this.showModalLogin()} className="button">Login</button>}
+                    {logged  && <button onClick = {this.logoutUser} className="button">Logout</button>}
+                </section>
+            </section>
+            {logged && <section className="board__panel-postits">
+                {
+                    actual_user.postits.map((postit) => <PostItComponent key={postit.id} id={postit.id} onUpdatePost = {this.editPostIt} onDeletePost={this.deletePostIt} message={postit.text}></PostItComponent>)
+                }
+            </section>}
+
+        </section>
+
+    }
+}
+
+export default BoardComponent
--- staff/joseluis-juste/postit-app/src/index.js
+++ staff/joseluis-juste/postit-app/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/styles.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import BoardComponent from './custom-components/board';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<BoardComponent />, document.getElementById('root'));
 serviceWorker.unregister();
