f898180f5541164afda93d0ce1ae5aa7e8fc12d6
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-10-18 12:57:46 +0200

    added login register and main

--- staff/joel-molero/postit-app/src/App.js
+++ staff/joel-molero/postit-app/src/App.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Register from './components/Register'
+import Login from './components/Login'
+import logic from './logic'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hola Mundo</h1>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+    state = { register: false, login: false }
+
+    handleRegister = () => {
+        this.setState({ register: true })
+    }
+
+    handleLogin = () => {
+        this.setState({ login: true })
+    }
+
+    handleRegisterClick = (name, surname, username, password) => {
+        logic.registerUser(name, surname, username, password)
+    }
+
+    handleLoginClick = (username, password) => {
+        try {
+        const userId = logic.loginUser(username, password)         
+        
+        console.log(userId)
+        } catch(err) {
+            console.error("incorrect credentials")
+        }
+    }
+        
+        
+        
+        /* , res => {
+        if (res === 'ok') ....
+        else ... 
+        })
+        o tambien con promises
+        .then(res => ...)
+        .catch(res => ...) */
+    
+
+    render() {
+        return <div>
+            {!this.state.register && !this.state.login && <section><button onClick={this.handleRegister}>Register</button> or <button onClick={this.handleLogin}>Login</button></section>}
+            {this.state.register && <Register onRegisterClick={this.handleRegisterClick} />}
+            {this.state.login && <Login onLoginClick={this.handleLoginClick} />}
+            {}
         </div>
-    );
-  }
+    }
 }
 
-export default App;
+export default App
---
+++ staff/joel-molero/postit-app/src/logic.js
@@ -0,0 +1,71 @@
+import data from './data'
+
+const { storage, Postit, User } = data
+
+const logic = {
+    createPostit(text) {
+        const postit = new Postit(text)
+
+        const postits = this.listPostits()
+
+        postits.push(postit)
+
+        this._persistPostits(postits)
+    },
+
+    listPostits() {
+        return JSON.parse(storage.getItem('postits'))
+    },
+
+    _persistPostits(postits) {
+        storage.setItem('postits', JSON.stringify(postits))
+    },
+
+    deletePostit(id) {
+        let postits = this.listPostits()
+
+        postits = postits.filter(postit => postit.id !== id)
+
+        this._persistPostits(postits)
+    },
+
+    updatePostit(id, text) {
+        let postits = this.listPostits()
+
+        const postit = postits.find(postit => postit.id === id)
+
+        postit.text = text
+
+        this._persistPostits(postits)
+    },
+
+    listUsers() {
+        return JSON.parse(storage.getItem('users'))
+    },
+
+    _persistUsers(users) {
+        storage.setItem('users', JSON.stringify(users))
+    },
+
+    registerUser(name, surname, username, password) {
+        const user = new User(name, surname, username, password)
+
+        const users = this.listUsers()
+
+        users.push(user)
+
+        this._persistUsers(users)
+    },
+
+    loginUser(username, password) {
+        const users = this.listUsers()
+
+        const user = users.find(user => user.username === username && user.password === password)
+
+        if (!user) throw Error('wrong credentials')
+
+        return user.id
+    }
+}
+
+export default logic
