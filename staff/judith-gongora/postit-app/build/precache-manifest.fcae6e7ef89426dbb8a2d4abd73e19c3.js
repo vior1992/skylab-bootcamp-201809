ca98a3e1b6617bae7eaef09107cc65de36696618
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-10-19 18:31:55 +0200

    update README

---
+++ staff/judith-gongora/postit-app/build/precache-manifest.fcae6e7ef89426dbb8a2d4abd73e19c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48740f56181756898bd8",
+    "url": "/static/js/main.48740f56.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "48740f56181756898bd8",
+    "url": "/static/css/main.bb6dfbad.chunk.css"
+  },
+  {
+    "revision": "afcf17ebab0d8462979b2f4994113e3b",
+    "url": "/index.html"
+  }
+];
