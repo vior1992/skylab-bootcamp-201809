cf62c859f51e11b964d5d092dca31f1dc83207a0
Paco <franfercant@gmail.com>
2018-10-19 17:44:14 +0200

    to the proyect

--- staff/francisco-fernandez/postit-app/src/components/Postits.js
+++ staff/francisco-fernandez/postit-app/src/components/Postits.js
@@ -4,32 +4,44 @@ import InputForm from './InputForm'
 import Post from './Post'
 
 class Postits extends Component {
-    state = { postits: logic.listPostitsByUser(this.props.userId) }
+    state = { postits: [] }
+
+    componentDidMount() {
+        console.log('Postits', 'componentDidMount')
+
+        const { userId, token } = this.props
+
+        logic.listPostitsByUser(userId, token)
+            .then(postits => { this.setState({ postits }) })
+    }
 
     handleSubmit = text => {
-        console.log('App', 'handleSubmit (setState)')
+        const { userId, token } = this.props
 
-        const { userId } = this.props
-
-        logic.createPostit(text, userId)
-
-        this.setState({ postits: logic.listPostitsByUser(userId) })
+        logic.createPostit(text, userId, token)
+            .then(() => logic.listPostitsByUser(userId, token))
+            .then(postits => this.setState({ postits }))
     }
 
     handleDeletePost = id => {
-        logic.deletePostit(id)
+        const { userId, token } = this.props
 
-        this.setState({ postits: logic.listPostitsByUser(this.props.userId) })
+        logic.deletePostit(id, userId, token)
+            .then(() => logic.listPostitsByUser(userId, token))
+            .then(postits => this.setState({ postits }))
     }
 
     handleUpdatePost = (id, text) => {
-        logic.updatePostit(id, text)
+        const { userId, token } = this.props
 
-        this.setState({ postits: logic.listPostitsByUser(this.props.userId) })
+        logic.updatePostit(id, text, userId, token)
+            .then(() => logic.listPostitsByUser(userId, token))
+            .then(postits => this.setState({ postits }))
+
     }
 
     render() {
-        console.log('App', 'render')
+        console.log('Postits', 'render')
 
         return <div>
             <h1>Post-It App <i className="fas fa-sticky-note"></i></h1>
