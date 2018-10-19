a6156636221c7fb1aa3a81572c2f5406eef44a32
Gianluca <gianluca.bragaglia@gmail.com>
2018-10-19 20:52:02 +0200

    update

--- staff/gianluca-bragaglia/postit-app/src/components/Landing.js
+++ staff/gianluca-bragaglia/postit-app/src/components/Landing.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Landing(props) {
     return <section>
-        <button onClick={props.onRegisterClick}>Register</button> or <button onClick={props.onLoginClick}>Login</button>
+        <button onClick={props.onRegisterClick}>Register</button> or 
+        <button onClick={props.onLoginClick}>Login</button>
     </section>
 }
 
