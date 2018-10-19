90d697414d9f34360dc72fecacd97b97c810c6a0
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-10-19 16:46:30 +0200

    added error handling

--- staff/joel-molero/postit-app/src/App.js
+++ staff/joel-molero/postit-app/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import Register from './components/Register'
 import Login from './components/Login'
-import Home from './components/Home'
+import Postits from './components/Postits'
+import Error from './components/Error'
 import logic from './logic'
 
 
 class App extends Component {
-    state = { register: false, login: false, userId: null }
+    state = { register: false, login: false, userId: null/*this.getUserId()*/, error: null }
+
+    // getUserId() {
+    //     const userId = sessionStorage.getItem('userId')
+
+    //     return userId ? parseInt(userId) : null
+    // }
 
     handleRegisterClick = () => {
         this.setState({ register: true })
@@ -19,31 +26,44 @@ class App extends Component {
     handleRegister = (name, surname, username, password) => {
         try {
             logic.registerUser(name, surname, username, password)
-
-            this.setState({ login: true, register: false })
+                .then(() => this.setState({ login: true, register: false, error: null }))
+                .catch(err => this.setState({ error: err.message }))
         } catch (err) {
-            console.error(err.message)
+            this.setState({ error: err.message })
         }
     }
 
     handleLogin = (username, password) => {
         try {
-            const userId = logic.authenticate(username, password)
-
-            this.setState({ userId, login: false, register: false })
+            logic.authenticate(username, password)
+                .then(({id, token}) => {
+                    sessionStorage.setItem('userId', id)
+                    sessionStorage.setItem('token', token)
+                    
+                    this.setState({ userId: id, token, login: false, register: false, error: null })
+                })
+                .catch(err => this.setState({ error: err.message }))
         } catch (err) {
-            console.error(err.message)
+            this.setState({ error: err.message })
         }
     }
 
+    handleLogoutClick = () => {
+        this.setState({ userId: null })
+
+        sessionStorage.removeItem('userId')
+    }
+
     render() {
-        const { register, login, userId } = this.state
+        const { register, login, userId, token, error } = this.state
 
         return <div>
             {!register && !login && !userId && <section><button onClick={this.handleRegisterClick}>Register</button> or <button onClick={this.handleLoginClick}>Login</button></section>}
             {register && <Register onRegister={this.handleRegister} />}
             {login && <Login onLogin={this.handleLogin} />}
-            {userId && <Home userId={userId} />}
+            {error && <Error message={error} />}
+            {userId && <section><button onClick={this.handleLogoutClick}>Logout</button></section>}
+            {userId && <Postits userId={userId} token={token} />}
         </div>
     }
 }
