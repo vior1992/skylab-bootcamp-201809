83302312477eb05ed85517a75154c37bc7bb247e
Ismael Haytam <anonismaa@gmail.com>
2018-10-19 17:54:36 +0200

    fix postit

--- staff/ismael-haytam/postit-app/src/components/auth/register.js
+++ staff/ismael-haytam/postit-app/src/components/auth/register.js
@@ -8,6 +8,8 @@ class Register extends Component {
 
         this.state = {
             registred: false,
+            logged: false,
+            login: false,
             name: '',
             email: '',
             password: '',
@@ -18,27 +20,37 @@ class Register extends Component {
     setEmail = event => this.setState({ email: event.target.value });
     setPassword = event => this.setState({ password: event.target.value });
 
-    registerUser = () => {
-        this.setState({ registred: true });
+    authUser = () => {
+        this.setState({ registred: true, logged: true, login: false });
+    };
+
+    showLogin = () => {
+        this.setState({ login: true });
     };
 
     render() {
         return <div className="lists">
-                {!this.state.registred &&
+                {!this.state.registred && !this.state.logged && !this.state.login  &&
                     <div>
                         <h4>Register</h4>
                         <input placeholder="name" value={this.state.name} type="text"  onChange={this.setName}/>
                         <input placeholder="email" value={this.state.email} type="email"  onChange={this.setEmail}/>
                         <input placeholder="password" value={this.state.password} type="password"  onChange={this.setPassword}/>
-                        <button onClick={this.registerUser}>Register</button>
+                        <button onClick={this.authUser}>Register</button>
+                        <button onClick={this.showLogin}>¿Ya tienes cuenta?</button>
                     </div>
                 }
-                {this.state.registred &&
+                {this.state.registred && this.state.logged && !this.state.login &&
                     <div className="lists-fix">
-                         <PostIt title="TODO" />
-                         <PostIt title="DOING" />
-                         <PostIt title="REVIEW" />
-                         <PostIt title="DONE" />
+                         <PostIt title="TODO"/>
+                    </div>
+                }
+                {this.state.login &&
+                    <div>
+                        <h4>Login</h4>
+                        <input placeholder="email" value={this.state.email} type="email"  onChange={this.setEmail}/>
+                        <input placeholder="password" value={this.state.password} type="password"  onChange={this.setPassword}/>
+                        <button onClick={this.authUser}>Register</button>
                     </div>
                 }
         </div>
