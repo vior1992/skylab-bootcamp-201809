98e151c908582c7f2b87b38d93b6709f16897f14
pepdbm7 <pepdbm7@gmail.com>
2018-10-21 18:34:46 +0200

    finished landing

--- staff/groups/upstream/spotify-cube/src/custom-components/landing/landing.jsx
+++ staff/groups/upstream/spotify-cube/src/custom-components/landing/landing.jsx
@@ -8,15 +8,18 @@ export default class Landing extends Component {
  
     render() {
 
-        return ( <div className="landing--parent">
+        return ( <div className="box-shadow">
+             <div className="landing--parent">
             <section className="landing--bg-img">
             </section>
             <section className="landing--content">
-                <h1 className="title--landing"><i class="fab fa-spotify"></i> Music<span className=""> is life</span></h1>
+                <h1 className="title--landing"><i class="fab fa-spotify"></i> Music <span className="span"> is life</span></h1>
                 <div className="landing--cube"><Cubito/></div>
-                <button className="btn btn-primary" onClick={this.props.onClickEnter}>Enter</button>
+                <button className="btn btn-warning" onClick={this.props.onClickEnter}>Enter</button>
             </section>
             </div>
+        </div>
+       
         )
     }
 }
