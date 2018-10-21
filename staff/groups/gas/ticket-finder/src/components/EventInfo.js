06cc239f4e1506936a949fc5927719a31e642750
Almu Iturgoyen <almudena.iturgoyen@gmail.com>
2018-10-21 13:37:57 +0200

    add log out profile button error

--- staff/groups/gas/ticket-finder/src/components/EventInfo.js
+++ staff/groups/gas/ticket-finder/src/components/EventInfo.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function Event(props) {
-    return <li>
-        <div className="card">
-            <a href="#"><img className="card-img-top" src={props.eventImgUrl } alt="Card cap" /></a>
+class EventInfo extends Component {
+    render() {
+    return <div className="card">
+            <img className="card-img-top" src={props.eventImgUrl } alt="Card cap" />
                 <div className="card-body">
                     <h5 className="card-title"> {props.eventName }</h5>
                     <p> { props.eventCity } </p>
                     <p className="card-text"><a target="blank" href= {props.eventUrl }>Get tickets</a></p>
                     {/* <p> { props.eventPrice }</p> */}
                 </div>
-        </div>       
-    </li>
-}     
-        
-export default Event
+        </div>
+    }
+}
+
+export default EventInfo
---
+++ staff/groups/gas/ticket-finder/src/components/Profile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Profile(props) {
+    return <p>Profile</p>
+}
+
+export default Profile
