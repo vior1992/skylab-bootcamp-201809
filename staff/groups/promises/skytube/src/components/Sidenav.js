015a14206d8c8e7e8113bf83175209c1afc32b3f
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-23 10:18:47 +0200

    model init

--- staff/groups/promises/skytube/src/components/Sidenav.js
+++ staff/groups/promises/skytube/src/components/Sidenav.js
@@ -3,20 +3,21 @@ import React from 'react'
 function Sidenav(props) {
     return <aside className="sidenav">
         <button className="sidenav__title" onClick={props.onClickFavourites}>Favourites</button>
-        <button className="sidenav__title" onClick={props.onClickWatchLater}>Watch Later</button>
         <button className="sidenav__title" onClick={props.onClickHistory}>History</button>
 
         <nav>
             <h2 className="sidenav__title">Playlists</h2>
             <ul className="sidenav__menu">
-                {props.playlists && props.playlists.length > 0 ? (
+                <li className="sidenav__item">
+                    <button onClick={props.onClickWatchLater}>Watch Later</button>
+                </li>
+
+                {props.playlists && props.playlists.length > 0 && (
                     props.playlists.map(playlist => {
                         return <li className="sidenav__item">
                             <button onClick={() => props.onClickPlaylist(playlist.id)}>{playlist.name}</button>
                         </li>
                     })
-                ) : (
-                    <li className="sidenav__item--blank">You don't have playlists currently</li>
                 )}
             </ul>
         </nav>
