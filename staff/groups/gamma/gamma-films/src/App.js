dbb6352458a0cf12452e937549e361b086e62157
Sergio <sergio.luz.fernandez@gmail.com>
2018-10-24 17:00:07 +0200

    session storage

--- staff/groups/gamma/gamma-films/src/App.js
+++ staff/groups/gamma/gamma-films/src/App.js
@@ -16,7 +16,11 @@ class App extends Component {
         loggedIn: false,
         user: ''
     }
-    componentDidMount = () => this.props.history.push('/')
+    componentDidMount = () => {
+        this.props.history.push('/')
+        const res = logic.loggedIn
+        this.setState({ loggedIn: res })
+    }
 
     handleLoginClick = () => this.props.history.push('/login')
 
@@ -50,12 +54,12 @@ class App extends Component {
         console.log('llama correctamente ' + id)
         let FavList = ''
         logic.listFavourites()
-        .then(res=>{
-            console.log(res)
-            FavList = res
-            logic.updateFavourites(res, id)
-            .then(res=> res)
-        })
+            .then(res => {
+                console.log(res)
+                FavList = res
+                logic.updateFavourites(res, id)
+                    .then(res => res)
+            })
     }
 
     render() {
@@ -63,11 +67,11 @@ class App extends Component {
         return <div className="body">
             <Route path="/" render={() => <Navbar onLogoClick={this.handleLogoClick} onLoginClick={this.handleLoginClick} onRegisterClick={this.handleRegisterClick} isLoggedIn={this.state.loggedIn} onLogoutClick={this.handleLogoutClick} />} />
 
-            <Route exact path="/register" render={() => !logic.loggedIn ? <Register history={this.props.history} /> : <Redirect to="/home" />} />
+            <Route exact path="/register" render={() => !logic.loggedIn ? <Register history={this.props.history} /> : <Redirect to="/" />} />
 
-            <Route exact path="/login" render={() => !logic.loggedIn ? <Login history={this.props.history} isLoggedIn={this.handleLoggedIn} /> : <Redirect to="/home" name={this.state.user.name} />} />
+            <Route exact path="/login" render={() => !logic.loggedIn ? <Login history={this.props.history} isLoggedIn={this.handleLoggedIn} /> : <Redirect to="/" name={this.state.user.name} />} />
 
-            <Route path="/" render={() => <Home isLoggedIn={this.state.loggedIn}  />} />
+            <Route path="/" render={() => <Home isLoggedIn={this.state.loggedIn} />} />
 
             {/* <Route path="/profile" render={() => !logic.loggedIn ? <Profile /> : <Redirect to="/profile" />} /> */}
 
