e78054707343b521db3791cc4ac5b714e10152f9
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-24 09:37:47 +0200

    some improvements

--- staff/groups/prototype/prototype-app/src/components/MovieDetail.js
+++ staff/groups/prototype/prototype-app/src/components/MovieDetail.js
@@ -12,6 +12,9 @@ class MovieDetail extends Component {
       .then(() => {
 
         if (JSON.stringify(logic._user) !== '{}') {
+
+          //Check another way to know if logic._user is an empty object or not
+
           let indexSeen = logic._user.seen.findIndex(movie => movie.id === this.state.movie.id)
 
           if (indexSeen !== -1) {
@@ -38,8 +41,16 @@ class MovieDetail extends Component {
           }
         } else this.setState({ warning: 'You should log in before to use that feature' })
       })
-    //Where to put that to guarantee that the warning message disappears? 
-    this.setState({ warning: null })
+      
+    }
+    
+    componentDidUpdate = (prevProps, prevState) => {
+      // if(prevState.seen !== this.state.seen || prevState.pending !== this.state.pending || prevState.favourite !== this.state.favourite || prevState.like !== this.state.like || prevState.unlike !== this.state.unlike) {
+      if(prevState.seen !== this.state.seen ) {
+        
+        //Where to put that to guarantee that the warning message disappears? 
+        this.setState({ warning: null })
+      }
 
   }
 
@@ -164,28 +175,32 @@ class MovieDetail extends Component {
     } else this.setState({ warning: 'You should log in before to use that feature' })
   }
 
+  componentDidCatch(errorString, errorInfo) {
+    this.setState({ error: errorString })
+    //What is error info???! 
+  }
 
   render() {
     return <div>
       {this.state.movie && <div>
-      <div className='movie-profile card container-fluid'>
-        {this.state.movie.backdrop_path ? <img className='backdrop card-img container' src={`https://image.tmdb.org/t/p/w780/${this.state.movie.backdrop_path}`} /> : <img src='https://dummyimage.com/780x439/cfcfcf/000.jpg' />}
-        <div className='movie-profile__card card-img-overlay media'>
-          {this.state.movie.poster_path ? <img className='card__poster mr-5' src={`https://image.tmdb.org/t/p/w185/${this.state.movie.poster_path}`} /> : <img src='https://dummyimage.com/185x278/cfcfcf/000.jpg' />}
-          <div className='card__info media-body'>
-            <p className='cardtitle mt-5 ml-4'>{this.state.movie.title}</p>
-            <div className='cardtext ml-4'>
-              <span>{this.state.movie.release_date.slice(0, 4)}</span>  <span> | </span>
-              <span>{`${this.state.movie.runtime}'`}</span>   <span> | </span>
-              <span>{`${this.state.movie.vote_average} / 10`}</span>
-              {this.state.movie.spoken_languages.map(languages => <p>{languages.name}</p>)}
-              {this.state.movie.production_countries.map(companies => <p>{companies.origin_country}</p>)}
-              {this.state.movie.budget !== 0 && <p>{`Budget $${this.state.movie.budget}`}</p>}
-              {this.state.movie.genres.map(genres => <div><span>{genres.name}</span><span> | </span></div>)}
+        <div className='movie-profile card container-fluid'>
+          {this.state.movie.backdrop_path ? <img className='backdrop card-img container' src={`https://image.tmdb.org/t/p/w780/${this.state.movie.backdrop_path}`} /> : <img src='https://dummyimage.com/780x439/cfcfcf/000.jpg' />}
+          <div className='movie-profile__card card-img-overlay media'>
+            {this.state.movie.poster_path ? <img className='card__poster mr-5' src={`https://image.tmdb.org/t/p/w185/${this.state.movie.poster_path}`} /> : <img src='https://dummyimage.com/185x278/cfcfcf/000.jpg' />}
+            <div className='card__info media-body'>
+              <p className='cardtitle mt-5 ml-4'>{this.state.movie.title}</p>
+              <div className='cardtext ml-4'>
+                <span>{this.state.movie.release_date.slice(0, 4)}</span>  <span> | </span>
+                <span>{`${this.state.movie.runtime}'`}</span>   <span> | </span>
+                <span>{`${this.state.movie.vote_average} / 10`}</span>
+                {this.state.movie.spoken_languages.map(languages => <p>{languages.name}</p>)}
+                {this.state.movie.production_countries.map(companies => <p>{companies.origin_country}</p>)}
+                {this.state.movie.budget !== 0 && <p>{`Budget $${this.state.movie.budget}`}</p>}
+                {this.state.movie.genres.map(genres => <div><span>{genres.name}</span><span> | </span></div>)}
+              </div>
             </div>
           </div>
         </div>
-      </div>
         <div className='container'>
           <div className='row'>
             <div className='col'>
@@ -199,6 +214,16 @@ class MovieDetail extends Component {
               </div>
             </div>
             <div className='sinopsis col d-flex flex-row justify-content-start mt-3 ml-5'>
+              
+              <div>
+              <a onClick={this.handleSeenClick}><i className="fa fa-eye"></i></a>
+        <a onClick={this.handlePendingClick}><i className="fa fa-clock-o"></i></a>
+        <a onClick={this.handleFavouriteClick}><i className="fa fa-star-o"></i></a>
+        <a onClick={this.handleLikeClick}><i className="fa fa-thumbs-up"></i></a>
+        <a onClick={this.handleUnlikeClick}><i className="fa fa-thumbs-down"></i></a>
+        {this.state.warning && <p>{this.state.warning}</p>}
+              </div>
+              
               <div>
                 <h3 className='sinopsis-title mt-0 mr-5'>Overview</h3>
                 <p className='sinopsis-text mr-5 text-justify'>{this.state.movie.overview}</p>
@@ -219,7 +244,8 @@ class MovieDetail extends Component {
             </div>
           </div>
         </div>
-        </div>}
+      </div>}
+      {this.state.error && <h3>{this.state.error}</h3>}
     </div>
   }
 }
@@ -248,29 +274,29 @@ class MovieDetail extends Component {
       </div>}
     </div>
       <div> */}
-    //   return <div>
-    //   {this.state.movie && <div>
-    //     <a onClick={this.handleBackClick}><i className="fa fa-arrow-left"></i></a>
-    //     {this.state.movie.backdrop_path? <img src={`https://image.tmdb.org/t/p/w780/${this.state.movie.backdrop_path}`} />: <img src='https://dummyimage.com/780x439/cfcfcf/000.jpg' />}
-    //     {this.state.movie.poster_path?<img src={`https://image.tmdb.org/t/p/w185/${this.state.movie.poster_path}`} />: <img src='https://dummyimage.com/185x278/cfcfcf/000.jpg' />}
-    //     <p>{this.state.movie.title}</p>
-    //     <p>{this.state.movie.release_date.slice(0, 4)}</p>
-    //     <p>{`${this.state.movie.runtime}'`}</p>
-    //     <p>{this.state.movie.vote_average}</p>
-    //     {this.state.movie.spoken_languages.map(languages => <p>{languages.name}</p>)}
-    //     {this.state.movie.production_countries.map(companies => <p>{companies.origin_country}</p>)}
-    //     {this.state.movie.budget !== 0 && <p>{`Budget $${this.state.movie.budget}`}</p>}
-    //     {this.state.movie.genres.map(genres => <p>{genres.name}</p>)}
-    //     <a onClick={this.handleSeenClick}><i className="fa fa-eye"></i></a>
-    //     <a onClick={this.handlePendingClick}><i className="fa fa-clock-o"></i></a>
-    //     <a onClick={this.handleFavouriteClick}><i className="fa fa-star-o"></i></a>
-    //     <a onClick={this.handleLikeClick}><i className="fa fa-thumbs-up"></i></a>
-    //     <a onClick={this.handleUnlikeClick}><i className="fa fa-thumbs-down"></i></a>
-    //     {this.state.warning && <p>{this.state.warning}</p>}
-    //     <p>{`Overview  ${this.state.movie.overview}`}</p>
-    //     {this.state.movie.credits.cast.slice(0, 5).map(cast => <div>{cast.profile_path? <img src={`https://image.tmdb.org/t/p/w92/${cast.profile_path}`} />: <img src='https://dummyimage.com/92x138/cfcfcf/000.jpg' />}<p>{cast.name}</p> </div>)}
+//   return <div>
+//   {this.state.movie && <div>
+//     <a onClick={this.handleBackClick}><i className="fa fa-arrow-left"></i></a>
+//     {this.state.movie.backdrop_path? <img src={`https://image.tmdb.org/t/p/w780/${this.state.movie.backdrop_path}`} />: <img src='https://dummyimage.com/780x439/cfcfcf/000.jpg' />}
+//     {this.state.movie.poster_path?<img src={`https://image.tmdb.org/t/p/w185/${this.state.movie.poster_path}`} />: <img src='https://dummyimage.com/185x278/cfcfcf/000.jpg' />}
+//     <p>{this.state.movie.title}</p>
+//     <p>{this.state.movie.release_date.slice(0, 4)}</p>
+//     <p>{`${this.state.movie.runtime}'`}</p>
+//     <p>{this.state.movie.vote_average}</p>
+//     {this.state.movie.spoken_languages.map(languages => <p>{languages.name}</p>)}
+//     {this.state.movie.production_countries.map(companies => <p>{companies.origin_country}</p>)}
+//     {this.state.movie.budget !== 0 && <p>{`Budget $${this.state.movie.budget}`}</p>}
+//     {this.state.movie.genres.map(genres => <p>{genres.name}</p>)}
+//     <a onClick={this.handleSeenClick}><i className="fa fa-eye"></i></a>
+//     <a onClick={this.handlePendingClick}><i className="fa fa-clock-o"></i></a>
+//     <a onClick={this.handleFavouriteClick}><i className="fa fa-star-o"></i></a>
+//     <a onClick={this.handleLikeClick}><i className="fa fa-thumbs-up"></i></a>
+//     <a onClick={this.handleUnlikeClick}><i className="fa fa-thumbs-down"></i></a>
+//     {this.state.warning && <p>{this.state.warning}</p>}
+//     <p>{`Overview  ${this.state.movie.overview}`}</p>
+//     {this.state.movie.credits.cast.slice(0, 5).map(cast => <div>{cast.profile_path? <img src={`https://image.tmdb.org/t/p/w92/${cast.profile_path}`} />: <img src='https://dummyimage.com/92x138/cfcfcf/000.jpg' />}<p>{cast.name}</p> </div>)}
 
-    //   </div>}
-    // </div>
+//   </div>}
+// </div>
 
 export default withRouter(MovieDetail)
