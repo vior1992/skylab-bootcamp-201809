57ced67ab194fbee8e309d73698a58d37ad763ab
Fede <fedegomezcucchiararo@gmail.com>
2018-10-24 18:00:36 +0200

    update spotifyLogic

--- staff/groups/upstream/spotify-cube/src/services/spotifylogic.js
+++ staff/groups/upstream/spotify-cube/src/services/spotifylogic.js
@@ -1,12 +1,12 @@
 
 const spotifyLogic = {
 
-    token: 'BQDXIQ31x9Z-B9lBldvxrgnXpea-vhcanJmcVonoR9019mJBzhN9R3vnxv4NLPpP7zEytSWrqKgiRgdO_7guZOmCQKIART5kee2n3RT-6HyOC3LsS_nm1CpvLdZItGf80d-JWH0sQlTbTXauTT3hrh9JCKf4Kj2iIDkWKIKipYb9TSUdgzolOeau3Fs9H0CH9JW5StT_3Zd9GI6z',
+    token: 'BQCNb5afPx0maW4h24sJbaD9_9rmu-PzvChuet2zfElaI-dWMIwZfX_6uKwM1viHiLTXAOrWx86P-FgL-pLHWnrKN9DuZgkckhidHl05zo30b7sDbLuRvOutC1DCZG_ei7yRHBc37vALrYJ8mTSZtbUd3U9q-imcsrMjy-CIKSFLdaYLEWzI-OnLhLpe1Gb26rmpbh-5Cb0Bi-cK',
 
     getArtistById(id) {
 
-        if(!(typeof id === 'string')) throw Error ('id is not a a string')
-        if(!(id.length)) throw Error ('id is empty or blank')
+        if(!(typeof id === 'string')) throw TypeError (`${id} is not a a string`)
+        if(!(id.length)) throw Error (`${id} is empty or blank`)
 
         return fetch(`https://api.spotify.com/v1/artists/${id}`, {
             method: 'GET',
@@ -17,13 +17,17 @@ const spotifyLogic = {
             // body: JSON.stringify({ name, surname, username, password })
         })
             .then(res => res.json())
-            .then(res => res)
-            .catch(res => res)
+            .then((res) => {
+                if(res.error) throw Error (res.error)
+                return res
+            })
+            
     },
 
     getArtists(query) {
 
-          if(!(query.trim().length)) throw Error ('Query cannot be empty')
+        if(!(typeof query === 'string')) throw TypeError (`${query} is not a a string`)
+        if(!(query.trim().length)) throw Error ('query cannot be empty')
         
         return fetch(`https://api.spotify.com/v1/search?q=${query}&type=artist`, {
             method: 'GET',
@@ -38,6 +42,10 @@ const spotifyLogic = {
     },
 
     getPlaylistsTracks(playlistId) {
+
+        if(!(typeof playlistId === 'string')) throw TypeError (`${playlistId} is not a a string`)
+        if(!(playlistId.trim().length)) throw Error ('query cannot be empty')
+        
         return fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
             method: 'GET',
             headers: {
@@ -47,7 +55,6 @@ const spotifyLogic = {
             // body: JSON.stringify({ name, surname, username, password })
         })
             .then(res => res.json())
-            .then(res => res)
     },
 
     getAlbumsByArtistId(artistId) {
@@ -75,20 +82,20 @@ const spotifyLogic = {
             .then(res => res)
     },
 
-    createPlaylist(name, description) {
+    createPlaylist(name) {
         return fetch(`https://api.spotify.com/v1/me/playlists`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8',
                 'Authorization': 'Bearer ' + this.token
             },
-            body: JSON.stringify({name: name, description: description})
+            body: JSON.stringify({name: name})
         })
             .then((res) => res.json())
             .then(res => res)
     }
 }
 
-export default spotifyLogic
+// export default spotifyLogic
 
-// module.exports = spotifyLogic
+module.exports = spotifyLogic
