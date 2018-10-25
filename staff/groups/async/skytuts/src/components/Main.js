b35333a080638f7aa9ad54b21e7dc34c54773197
Ismael Haytam <anonismaa@gmail.com>
2018-10-25 09:07:43 +0200

    fix bug - particles

--- staff/groups/async/skytuts/src/components/Main.js
+++ staff/groups/async/skytuts/src/components/Main.js
@@ -50,11 +50,11 @@ class Main extends Component {
         const data = JSON.parse(sessionStorage.getItem('courses'))
         if (data) {
             this.setState({ 
-                courses: data.courses || [],
+                courses: data.courses.slice(0, 6) || [],
                 tracks: data.tracks || [],
             })
         } else {
-            this.listCourses();
+            this.listCourses().slice(0, 6);
         }
         
     }
