a76d25784cba0f9d4a884302d4d7e784e2daa877
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-25 22:41:10 +0200

    small change

--- staff/groups/prototype/prototype-app/src/components/MoviesList.js
+++ staff/groups/prototype/prototype-app/src/components/MoviesList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const MoviesList = props => {
-  console.log(props)
   return (
     <div className="row">
       {props.movies.map(movie => {
