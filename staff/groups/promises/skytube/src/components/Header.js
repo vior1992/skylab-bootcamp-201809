d73004c080788b928a7b461e4c2cd547fa798197
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-25 14:03:39 +0200

    landing

--- staff/groups/promises/skytube/src/components/Header.js
+++ staff/groups/promises/skytube/src/components/Header.js
@@ -3,6 +3,15 @@ import SignUp from './SignUp'
 
 function Header(props) {
     return <header className="header">
+        <section className="header__hero">
+            <h1 className="header__title">Enjoyable.</h1>
+            <p className="header__text">
+                Skytube makes easy to enjoy your favourite music videos.
+                With one click, you can save videos to watch later or add them to your favourites.
+                Also, create your own playlists and have them always available to listen your favourite songs.
+                Enjoy the music and dance like no one is watching!
+            </p>
+        </section>
         <SignUp error={props.error} onSubmit={props.onSubmitSignUp}/>
     </header>
 }
