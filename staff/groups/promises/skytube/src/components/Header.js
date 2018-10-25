589e01335e31c1fbabac9b5342d5c80f6920e9fb
vior1992 <vior1992@gmal.com>
2018-10-25 10:38:26 +0200

    mod landing styles

--- staff/groups/promises/skytube/src/components/Header.js
+++ staff/groups/promises/skytube/src/components/Header.js
@@ -2,12 +2,10 @@ import React from 'react'
 import SignUp from './SignUp'
 
 function Header(props) {
-    return <header className="app-header">
-        <div className="container">
-            <img className="header__logo" src="/img/skytube.logo.png" alt="logo"></img>
-            <h1 className="header__title">Skytube</h1>
+    return <header>
+        <div>
+            <SignUp onSubmit={props.onSubmitSignUp}/>
         </div>
-        <SignUp onSubmit={props.onSubmitSignUp}/>
     </header>
 }
 
