e092d9eab836103c49f83af4ca5cf0133c601194
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-10-26 10:50:08 +0200

    landing

--- staff/groups/promises/skytube/src/components/Header.js
+++ staff/groups/promises/skytube/src/components/Header.js
@@ -6,9 +6,11 @@ function Header(props) {
         <section className="header__hero">
             <h1 className="header__title">Enjoyable.</h1>
             <p className="header__text">
-                Skytube makes easy to enjoy your favourite music videos.<br/>
+                Skytube makes easy to enjoy your favourite music videos.
                 With one click, you can save videos to watch later or add them to your favourites.
-                Also, create your own playlists and have them always available.<br/>
+                Also, create your own playlists and have them always available.
+            </p>
+            <p className="header__text">
                 Enjoy the music and dance like no one is watching!
             </p>
         </section>
