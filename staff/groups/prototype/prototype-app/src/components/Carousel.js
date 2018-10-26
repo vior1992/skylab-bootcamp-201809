90b558b049f03ca4349c1856576db24a1887cf29
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-10-26 12:41:32 +0200

    last improvements

--- staff/groups/prototype/prototype-app/src/components/Carousel.js
+++ staff/groups/prototype/prototype-app/src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import logic from '../logic'
 import { Link } from 'react-router-dom'
 import $ from 'jquery'
@@ -10,25 +10,31 @@ class Carousel extends Component {
   componentDidMount = () => {
     let slick
 
-    switch(this.props.listType) {
+    switch (this.props.listType) {
       case 'now_playing':
         slick = logic.retrieveInTheatre()
           .then(movies => {
             this.setState({ movies: logic._inTheatreMovies })
           })
-      break
+          .catch(error => this.setState({ error: error.message }))
+
+        break
       case 'popular':
-      slick = logic.retrievePopular()
+        slick = logic.retrievePopular()
           .then(movies => {
             this.setState({ movies: logic._popularMovies })
           })
-      break
+          .catch(error => this.setState({ error: error.message }))
+
+        break
       case 'trending':
-      slick = logic.retrieveTrending()
+        slick = logic.retrieveTrending()
           .then(movies => {
             this.setState({ movies: logic._trendingMovies })
           })
-      break
+          .catch(error => this.setState({ error: error.message }))
+
+        break
     }
 
     slick.then(() => {
@@ -40,24 +46,24 @@ class Carousel extends Component {
         centerMode: true,
         slidesToShow: 4,
         responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-          }
-        },
-        {
-          breakpoint: 990,
-          settings: {
-            slidesToShow: 2,
-          }
-        },
-        {
-          breakpoint: 760,
-          settings: {
-            slidesToShow: 1,
-          }
-        }]
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 990,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 760,
+            settings: {
+              slidesToShow: 1,
+            }
+          }]
       })
     })
   }
@@ -73,15 +79,16 @@ class Carousel extends Component {
           <div className="row">
             <h5 className="font-weight-bold my-4">{this.props.title}</h5>
           </div>
+          {this.state.error && <h3 className='text-center h5 mt-5 alert-danger'>{this.state.error}</h3>}
           <div className="row carousel" ref={el => this.el = el}>
-            { this.state.movies && this.state.movies.map(movie => {
+            {this.state.movies && this.state.movies.map(movie => {
               return (
                 <div className="carousel-img" key={movie.id}>
-                  { movie.poster_path && <Link to={`/movie/${movie.id}`}><img className="img-fluid img-medium-rounded" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} /></Link> }
-                  { !movie.poster_path && <Link to={`/movie/${movie.id}`}><img className="img-fluid img-medium-rounded" src="https://dummyimage.com/240x360/707070&text=+" /></Link> }
+                  {movie.poster_path && <Link to={`/movie/${movie.id}`}><img className="img-fluid img-medium-rounded" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} /></Link>}
+                  {!movie.poster_path && <Link to={`/movie/${movie.id}`}><img className="img-fluid img-medium-rounded" src="https://dummyimage.com/240x360/707070&text=+" /></Link>}
                 </div>
               )
-            }) }
+            })}
           </div>
         </div>
       </section>
--- staff/groups/prototype/prototype-app/src/components/ListedMovies.js
+++ staff/groups/prototype/prototype-app/src/components/ListedMovies.js
@@ -12,22 +12,35 @@ class ListedMovies extends Component {
             case 'trending':
                 logic.retrieveTrending()
                     .then(movies => { this.setState({ movies, title: 'Trending movies' }) })
+                    .catch(error => this.setState({ error: error.message }))
+
                 break
             case 'now_playing':
                 logic.retrieveInTheatre()
                     .then(movies => { this.setState({ movies, title: 'Now playing' }) })
+                    .catch(error => this.setState({ error: error.message }))
+
                 break
             case 'popular':
                 logic.retrievePopular()
                     .then(movies => { this.setState({ movies, title: 'Popular movies' }) })
+                    .catch(error => this.setState({ error: error.message }))
+
                 break
             default:
-                logic.retrieveMovies(this.props.kind, this.state.page)
-                    .then(movies => {
-                        let query = logic.beautifyQuery(this.props.kind)
-                        this.setState({ movies, title: query }) })               
-                
-                this.setState({ search: true })
+                try {
+                    logic.retrieveMovies(this.props.kind, this.state.page)
+                        .then(movies => {
+                            let query = logic.beautifyQuery(this.props.kind)
+                            this.setState({ movies, title: query })
+                        })
+                        .catch(error => this.setState({ error: error.message }))
+
+
+                    this.setState({ search: true })
+                } catch (error) {
+                    this.setState({ error: 'something went wrong, try again please' })
+                }
         }
     }
 
@@ -37,25 +50,38 @@ class ListedMovies extends Component {
                 case 'trending':
                     logic.retrieveTrending()
                         .then(movies => { this.setState({ movies, title: 'Trending movies' }) })
+                        .catch(error => this.setState({ error: error.message }))
+
                     break
                 case 'now_playing':
                     logic.retrieveInTheatre()
                         .then(movies => { this.setState({ movies, title: 'Now playing' }) })
+                        .catch(error => this.setState({ error: error.message }))
+
                     break
                 case 'popular':
                     logic.retrievePopular()
                         .then(movies => { this.setState({ movies, title: 'Popular movies' }) })
+                        .catch(error => this.setState({ error: error.message }))
+
                     break
                 default:
-                    logic.retrieveMovies(nextProps.kind)
-                        .then(movies => { 
-                            let query = logic.beautifyQuery(nextProps.kind)
- 
-                            this.setState({ movies, title: query }) })
-                            this.setState({ search: true })
+                    try {
+                        logic.retrieveMovies(nextProps.kind)
+                            .then(movies => {
+                                let query = logic.beautifyQuery(nextProps.kind)
+
+                                this.setState({ movies, title: query })
+                            })
+                            .catch(error => this.setState({ error: error.message }))
+
+                        this.setState({ search: true })
+                    } catch (error) {
+                        this.setState({ error: 'something went wrong, try again please' })
+                    }
             }
         }
- 
+
         this.setState({ page: '1' })
     }
 
@@ -63,10 +89,10 @@ class ListedMovies extends Component {
         let pages = []
         let maxPages = this.state.movies.total_pages
 
-        if(maxPages > 10) maxPages = 10
+        if (maxPages > 10) maxPages = 10
 
-        for(let i = 1; i <= maxPages; i++) {
-            if(i == this.state.page) {
+        for (let i = 1; i <= maxPages; i++) {
+            if (i == this.state.page) {
                 pages.push(<li className="page-item active"><a className="page-link" href="#" onClick={this.handleClick}>{i}</a></li>)
             } else {
                 pages.push(<li className="page-item"><a className="page-link" href="#" onClick={this.handleClick}>{i}</a></li>)
@@ -81,22 +107,42 @@ class ListedMovies extends Component {
 
         const page = event.target.innerText
 
-        switch(this.props.kind) {
+        switch (this.props.kind) {
             case 'trending':
-                logic.retrieveTrending(page)
-                .then(movies => this.setState({ page, movies }))
-            break
+                try {
+                    logic.retrieveTrending(page)
+                        .then(movies => this.setState({ page, movies }))
+                        .catch(error => this.setState({ error: error.message }))
+                } catch (error) {
+                    this.setState({ error: 'something went wrong, try again please' })
+                }
+                break
             case 'now_playing':
-                logic.retrieveInTheatre(page)
-                .then(movies => this.setState({ page, movies }))
-            break
+                try {
+                    logic.retrieveInTheatre(page)
+                        .then(movies => this.setState({ page, movies }))
+                        .catch(error => this.setState({ error: error.message }))
+                } catch (error) {
+                    this.setState({ error: 'something went wrong, try again please' })
+                }
+                break
             case 'popular':
-                logic.retrievePopular(page)
-                .then(movies => this.setState({ page, movies }))
-            break
+                try {
+                    logic.retrievePopular(page)
+                        .then(movies => this.setState({ page, movies }))
+                        .catch(error => this.setState({ error: error.message }))
+                } catch (error) {
+                    this.setState({ error: 'something went wrong, try again please' })
+                }
+                break
             default:
-                logic.retrieveMovies(this.props.kind, page)
-                .then(movies => this.setState({ page, movies }))
+                try {
+                    logic.retrieveMovies(this.props.kind, page)
+                        .then(movies => this.setState({ page, movies }))
+                        .catch(error => this.setState({ error: error.message }))
+                } catch (error) {
+                    this.setState({ error: 'something went wrong, try again please' })
+                }
         }
 
         $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -105,24 +151,25 @@ class ListedMovies extends Component {
     render() {
         return (
             <div>
-                { this.state.search && <Search /> }
-                { this.state.movies && <section className="list-movies">
+                {this.state.search && <Search />}
+                {this.state.movies && <section className="list-movies">
                     <div className="container">
                         <div className="row">
                             <h3 className="font-weight-bold mt-5 mb-4">{this.state.title}</h3>
                         </div>
+                        {this.state.error && <h3 className='text-center h5 mt-5 alert-danger'>{this.state.error}</h3>}
                         <MoviesList movies={this.state.movies.results} pages={this.state.movies.total_pages} currentPage={this.state.page} />
                         <div className="row justify-content-center">
                             <nav className="my-3">
                                 <ul className="pagination">
                                     {/* <li className="page-item"><a className="page-link" href="#">Previous</a></li> */}
-                                    { this.pagination().map(page => page) }
+                                    {this.pagination().map(page => page)}
                                     {/* <li className="page-item"><a className="page-link" href="#">Next</a></li> */}
                                 </ul>
                             </nav>
                         </div>
                     </div>
-                </section> }
+                </section>}
             </div>
         )
     }
