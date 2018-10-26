cbc1e9971d30a20e6013d19c70a5f59f5ec1489c
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-10-26 13:12:56 +0200

    add README

--- staff/groups/git-club/pintegram-app/src/components/Error.js
+++ staff/groups/git-club/pintegram-app/src/components/Error.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 function Error(props) {
-    return <div className="div__error">
-        <div className="div__x">
-            <i onClick={props.onErrorClose} className="far fa-times-circle x"></i>
-        </div>
-        <div className="div__error-center">
-            <p className="error">{props.message}</p>
+    return  <div className="div__error">
+        <div className="background">
+            <div className="error__center">
+                <div className="div__x">
+                    <i onClick={props.onErrorClose} className="far fa-times-circle x"></i>
+                </div>
+                <p className="error">{props.message}</p>
+            </div>
         </div>
     </div>
 }
