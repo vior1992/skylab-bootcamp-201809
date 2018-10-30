33a57658b370274108ebff30a0d7e6f1f7d06952
manuelbarzi <manuelbarzi@gmail.com>
2018-10-30 16:04:43 +0100

    update learn you node

--- staff/manuel-barzi/learnyounode/10-time-server/index.js
+++ staff/manuel-barzi/learnyounode/10-time-server/index.js
@@ -1,13 +1,22 @@
 const net = require('net')
+const strftime = require('strftime')
 
 const { argv: [, , port] } = process
 
 const server = net.createServer(socket => {
-    const date = 'hola mundo\n'
+    const date = new Date()
 
-    socket.write(date)
-    socket.write('hello world\n')
-    socket.write('...\n')
+    // expected format YYYY-MM-DD hh:mm
+
+    const formattedDate = strftime('%F %H:%M\n', date)
+
+    socket.end(formattedDate)
 })
 
+function digits(num) {
+    return num < 10 ? `0${num}` : `${num}`
+}
+
 server.listen(port)
+
+// $ nc localhost 8080
