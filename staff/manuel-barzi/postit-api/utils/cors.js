982f4c2a1bc9f6d8da7c28a8181b3eb77e134c73
manuelbarzi <manuelbarzi@gmail.com>
2018-11-07 16:15:20 +0100

    update postit api

--- staff/manuel-barzi/postit-api/utils/cors.js
+++ staff/manuel-barzi/postit-api/utils/cors.js
@@ -1,6 +1,8 @@
 function cors(req, res, next) {
-    res.set('Access-Control-Allow-Origin', '*')
-    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    })
 
     next()
 }
