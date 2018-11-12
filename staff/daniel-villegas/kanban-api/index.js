253e86225fbdb357ea48da8be047e5c488baa950
Daniel Villegas <dani@MacBook-Pro-de-Daniel.local>
2018-11-12 18:21:40 +0100

    change data to moongose

--- staff/daniel-villegas/kanban-api/index.js
+++ staff/daniel-villegas/kanban-api/index.js
@@ -1,25 +1,17 @@
 require('dotenv').config()
 
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose')
 const express = require('express')
 const package = require('./package.json')
 const router = require('./routes')
 const cors = require('./utils/cors')
-const { User } = require('./data')
+
 
 const { env: { PORT, MONGO_URL } } = process
 
-const client = new MongoClient(MONGO_URL, { useNewUrlParser: true })
-
-client.connect()
+mongoose.connect(MONGO_URL, { useNewUrlParser: true })
     .then(() => {
         console.log(`db server running at ${MONGO_URL}`)
-
-        const db = client.db('kanban')
-
-        users = db.collection('users')
-
-        User._collection = users
 
         const { argv: [, , port = PORT || 8080] } = process
 
