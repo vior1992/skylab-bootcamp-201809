034181f5c60501ef673892d672720410d0384e64
Daniel Villegas <dani@MacBook-Pro-de-Daniel.local>
2018-11-18 17:33:29 +0100

    modify template and styles

--- staff/daniel-villegas/partyup/partyup-app/src/App.js
+++ staff/daniel-villegas/partyup/partyup-app/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router'
 import logo from './logo.svg';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>HELLO</h1>
-      </div>
-    );
+    return <div>
+      <Route exact path="/landing" />
+      <Route path="/register" />
+      <Route path="/login" />
+      <Route path="/home" />
+      <Route path="/profile" />
+      <Route path="/create-partyup" />
+      <Route path="/partyup" />
+      <Route path="/search-partyups" />
+    </div>
   }
 }
 
--- staff/daniel-villegas/partyup/partyup-app/src/views/landing.js
+++ staff/daniel-villegas/partyup/partyup-app/src/views/landing.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Landing extends Component {
   render() {
     return <div>
-        <header class="header">
-            <div class="header__items">
-                <a class="header__logo" href="./landing.html">logo</a>
+        <header className="header">
+            <div className="header__items">
+                <a className="header__logo" href="./landing.html">logo</a>
                 
-                <div class="header__ancores">
+                <div className="header__ancores">
                     <a href="./login.html">Iniciar sesion</a>
                     <a href="./register.html">Registrarse</a>
                 </div>
@@ -15,33 +15,33 @@ class Landing extends Component {
         </header>
 
         <main>
-            <section class="main__videosection">
-                <video class="main__video" src=""></video>
-                <div class="main__register">
+            <section className="main__videosection">
+                <video className="main__video" src=""></video>
+                <div className="main__register">
                     <h2>¿Quieres fiesta?</h2>
                     <p>Encuentrala con Partyup</p>
                     <button onclick="">Registrarse</button>
                 </div>
             </section>
-            <section class="partyups">
-                <div class="partyups__titles">
+            <section className="partyups">
+                <div className="partyups__titles">
                     <h3>Proximos Partyups</h3>
                     <a href="login.html">Ver todo</a>
                 </div>
-                <div class="partyups__list">
-                    <div class="partyups__event">
+                <div className="partyups__list">
+                    <div className="partyups__event">
                         <p>Sabado, 17 de noviembre 8:00</p>
                         <h4>Skylabeers</h4>
                         <p>Fiesta en skylab con cerveza</p>
                         <p>Creado por: Dani</p>
                     </div>
-                    <div class="partyups__event">
+                    <div className="partyups__event">
                         <p>Sabado, 18 de noviembre 8:00</p>
                         <h4>Skylabeers</h4>
                         <p>Fiesta en skylab con cerveza</p>
                         <p>Creado por: Dani</p>
                     </div>
-                    <div class="partyups__event">
+                    <div className="partyups__event">
                         <p>Sabado, 19 de noviembre 8:00</p>
                         <h4>Skylabeers</h4>
                         <p>Fiesta en skylab con cerveza</p>
@@ -51,7 +51,7 @@ class Landing extends Component {
             </section> 
         </main>
         
-        <footer class="footer">
+        <footer className="footer">
             <p>© 2018 Partyup  Partyup es una subsidiaria totalmente controlada por Dani Companies Inc.</p>
         </footer>
     </div>
