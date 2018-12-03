815e2a588dadd3cd06c8da3752c4bb41b10d17ee
manuelbarzi <manuelbarzi@gmail.com>
2018-12-03 16:09:07 +0100

    boost partyup data

--- staff/daniel-villegas/partyup/partyup-api/index.js
+++ staff/daniel-villegas/partyup/partyup-api/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { mongoose } = require('data')
+const { mongoose } = require('partyup-data')
 const express = require('express')
 const cors = require('./utilities/cors')
 const package = require('./package.json')
